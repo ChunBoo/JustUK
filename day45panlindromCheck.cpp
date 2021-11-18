ca171a4ebf81f76688ab1cf5681d611a546d1015
cbliu <chunboo.liu@gmail.com>
2021-11-18 10:13:46 +0800

    20211118 panlindromCheck for given string

---
+++ day45panlindromCheck.cpp
@@ -0,0 +1,44 @@
+//given one string,to check if those chars can be a panlindrom 
+// basic method,check each char's count,if one char's count is odd and larger than 1,than can not be a panlindrom
+
+#include<iostream>
+#include<string>
+#include<map>
+bool check(const std::string& s)
+{
+    if(s.empty()||s.size()==1 )
+        return true;
+    std::map<char,int> countMap{};
+
+    for(auto i:s)
+    {
+        auto search=countMap.find(i);
+        if(search!=countMap.end())
+            countMap[i]+=1;
+        else    
+            countMap[i]=1;
+    }
+
+    //traverse the map again
+    int oddCnt=0;
+    for(auto i: countMap)
+    {
+        int cnt=i.second;
+        if(cnt&1==1)
+        {   
+            oddCnt++;
+            if(oddCnt>1)
+                return false;
+        }
+    }
+    return true;
+}
+
+
+int main()
+{
+    std::string s{"nonoaab"};
+    bool retValue=check(s);
+    std::cout<<s<<" is a panlindrome?"<<(retValue?"Yes":"No")<<'\n';
+    return 0;
+}
