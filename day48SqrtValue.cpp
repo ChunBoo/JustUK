0f99c2699d2d1fdd6380ebc60a3d486960b7b148
cbliu <chunboo.liu@gmail.com>
2021-11-19 10:49:47 +0800

    updated the codes with correct data type of float,previous is int.

--- day48SqrtValue.cpp
+++ day48SqrtValue.cpp
@@ -1,14 +1,14 @@
 //using bs to compute the sqrt(x)
 #include<iostream>
 #include<cmath>
-float mySqrt(int x)
+float mySqrt(float x)
 {
     if(x<0)
     {
         std::cout<<"X value is error,should be positive.\n";
         return 0;
     }
-    int lo=0,hi=0;
+    float lo=0,hi=0;
     if(x<1)
         hi=1;
     else
@@ -17,13 +17,13 @@ float mySqrt(int x)
     float cur=mid*mid;
     while(std::abs(cur-x)>1e-5)
     {
-        mid=(lo+hi)/2;
-        cur=mid*mid;
+ 
         if(cur<=x)
             lo=mid;
         else
             hi=mid;
-
+       mid=(lo+hi)/2.0;
+        cur=mid*mid;
     }
     return mid;
 }
@@ -31,7 +31,7 @@ float mySqrt(int x)
 int main()
 {
 
-    int x=3;
+    float x=2;
     float retValue=mySqrt(x);
     std::cout<<"result: "<<retValue<<'\n';
     return 0;
