8e72f23dd9b72fe493c83d507d5f246b8c451874
cbliu <chunboo.liu@gmail.com>
2021-11-26 11:24:09 +0800

    20211136 add the codes of building BST from a sorted list

---
+++ day65-buildABSTFromSortedList.cpp
@@ -0,0 +1,40 @@
+//build a bst from a sorted list
+
+#include<iostream>
+#include<vector>
+
+#include"treeNode.h"
+
+TreeNode* buildBSTFromSortedList(std::vector<int>& nums)
+{
+    if(nums.empty())
+        return nullptr;
+
+    int mid=nums.size()/2;
+    int tsize=nums.size();
+    TreeNode* root=new TreeNode(nums[mid]);
+    std::vector<int> leftPart{};
+    std::vector<int> rightPart{};
+    for(int i=0;i<mid;++i)
+    {
+        leftPart.push_back(nums[i]);
+    }
+    for(int i=mid+1;i<nums.size();++i)
+    {
+        rightPart.push_back(nums[i]);
+    }
+    root->left=buildBSTFromSortedList(leftPart);
+    root->right=buildBSTFromSortedList(rightPart);
+    return root;
+}
+
+
+int main()
+{
+    std::vector<int> nums{1,2,3,4,5,6};
+    TreeNode* root=buildBSTFromSortedList(nums);
+    root->printTreeInOrder(root);
+    std::cout<<"\npreOrder:\n";
+    root->printTreeNodePreOrder(root);
+    return 0;
+}
