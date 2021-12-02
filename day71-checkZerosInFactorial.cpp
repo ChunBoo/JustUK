c0ac1d81aef00ebfe6bf8213ce601c5fc49651e7
cbliu <chunboo.liu@gmail.com>
2021-12-02 10:39:23 +0800

    20211202 added the codes of checking zeros in a number

---
+++ day71-checkZerosInFactorial.cpp
@@ -0,0 +1,25 @@
+//check how many zeros in a factorial number,likes,120 will return 1
+
+//method is check the numbers of 5;
+
+#include<vector>
+#include<iostream>
+
+int checkZerosInFactorialNumberTail(int n)
+{
+    int ans=0;
+    while(n>0)
+    { 
+        n/=5;
+        ans+=n;
+    }
+    return ans;
+}
+
+int main()
+{
+    int n=14;
+    std::cout<<checkZerosInFactorialNumberTail(n)<<'\n';
+
+    return 0;
+}
