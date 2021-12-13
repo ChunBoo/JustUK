3d7811a952676251ff2a5e3cd0dd7807ec626cea
Chunbo Liu <cbliu@12sigma.ai>
2021-12-13 22:28:37 +0800

    20211213   sameLeafLevel

---
+++ day77-leavesLevelUsingDFS.cpp
@@ -0,0 +1,39 @@
+// 深度优先搜索算法判断某棵树的叶子节点是否在同一个level上
+
+#include<iostream>
+#include<set>
+#include "treeNode.h"
+void dfs(TreeNode* root,int curLevel,std::set<int>& s)
+{
+    if(!root)
+        return;
+    if(!root->left&&!root->right)
+    {
+        s.insert(curLevel);
+    }
+    dfs(root->left,curLevel+1,s);
+    dfs(root->right,curLevel+1,s);
+
+}
+bool sameLeavesLevel(TreeNode* root)
+{
+    std::set<int> s{};
+    dfs(root,0,s);
+    return s.size()==1;
+}
+
+
+int main()
+{
+    TreeNode* root=new TreeNode(4);
+    TreeNode* left=new TreeNode(3);
+    // left->addLeftChild(2);
+    root->left=left;
+    TreeNode* right=new TreeNode(5);
+    root->right=right;
+    root->printTreeInOrder(root);
+    std::cout<<'\n';
+    bool sameLevel=sameLeavesLevel(root);
+    std::cout<<sameLevel<<'\n';
+    return 0;
+}
