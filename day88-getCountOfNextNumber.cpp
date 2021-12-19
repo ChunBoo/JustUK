210c960c37a81ffbc03b3fb6a3cf4aef0d4a0043
Chunbo Liu <cbliu@12sigma.ai>
2021-12-19 11:18:36 +0800

    20211219 update the codes of getCountOfNextNubmer.cpp

--- day88-getCountOfNextNumber.cpp
+++ day88-getCountOfNextNumber.cpp
@@ -3,28 +3,35 @@
 #include<iostream>
 #include<set>
 #include<vector>
-
+#include<map>
 int getNextNumberCount(std::vector<int>& nums)
 {
     if(nums.empty())
         return 0;
     
-    std::set<int> s{};
+    std::map<int,int> m{};
     for(auto i:nums)
-        s.insert(i);
+    {
+        if(m.find(i)==m.end())
+        {
+            m[i]=1;
+        }
+        else
+            m[i]+=1;
+    }
     int ans=0;
     for(int i=0;i<nums.size();++i)
     {
         int nextValue=nums[i]+1;
-        if(s.find(nextValue)!=s.end())
-            ans+=1;
+        if(m.find(nextValue)!=m.end())
+            ans+=m[nextValue];
     }
     return ans;
 }
 
 int main()
 {
-    std::vector<int> nums{1,2,3,5,7};
+    std::vector<int> nums{1,2,2,3,5,7};
     int count=getNextNumberCount(nums);
     std::cout<<"Result is: "<<count<<'\n';
     return 0;
