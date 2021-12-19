0d535105c94de97d88771e05f1e8f070f6ec8ecf
Chunbo Liu <cbliu@12sigma.ai>
2021-12-19 21:26:06 +0800

    20211219 ugly number

---
+++ day90-uglyNumber.cpp
@@ -0,0 +1,29 @@
+//given a number to check it is a ugly number or not
+//ugly number is a number which can just divided by 2,3,5
+
+#include<iostream>
+#include<vector>
+bool isUglyNumber(int n)
+{
+    if(n<=0)
+        return false;
+    
+    std::vector<int> basicNumber{2,3,5};
+
+    for(auto i:basicNumber)
+    {
+        while(n>1 && n%i==0)
+        {
+            n/=i;
+        }
+    }
+    return n==1;
+}
+
+int main()
+{
+    int n=26;
+    std::cout<<n<<" is a ugly number? "<<isUglyNumber(n)<<'\n';
+
+    return 0;
+}
