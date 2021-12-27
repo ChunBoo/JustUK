38f895caccb6580ad298b2a3ae6c94b1c494933f
cbliu <chunboo.liu@gmail.com>
2021-12-27 09:44:16 +0800

    1227 temp changes

--- 94-confusingNumber.cpp
+++ 94-confusingNumber.cpp
@@ -3,28 +3,47 @@
 #include<iostream>
 #include<string>
 
+char convert(char i)
+{
+    if(i=='6')
+        return '9';
+    else if(i=='9')
+        return '6';
+    else if(i=='0'||i=='1'||i=='8')
+        return i;
+    return '-1';
+
+}
+
 bool isConfusingNumber(int n)
 {
     std::string s{std::to_string(n)};
     std::string temp{};
     for(auto i:s)
     {
-        if(i=='6')
-            temp+='9';
-        else if(i=='9')
-            temp+='6';
-        else if(i=='0'||i=='1')
-            temp+=i;
+        char newChar=convert(i);
+        if(newChar=='-1')
+            return false;
+        temp+=newChar;
     }
-    
+    std::cout<<"temp number is:"<<temp<<'\n';
+    return temp!=s;
 }
 
 int main()
 {
-    int d=123;
-    std::string s{std::to_string(d)};
-    // std::cout<<"string s: "<<s<<'\n';
-    for(auto i:s)
-        std::cout<<i<<'-';
+    int num=10986;
+    bool confusingNumber=isConfusingNumber(num);
+    std::cout<<num<<" is a confusing number? "<<!confusingNumber<<'\n';
     return 0;
 }
+
+// int main()
+// {
+//     int d=123;
+//     std::string s{std::to_string(d)};
+//     // std::cout<<"string s: "<<s<<'\n';
+//     for(auto i:s)
+//         std::cout<<i<<'-';
+//     return 0;
+// }
--- 95-perfectNumber.cpp
+++ 95-perfectNumber.cpp
@@ -18,9 +18,26 @@ bool isPerfectNumber(int n)
     return sum==n;
 }
 
+bool isPerfectNumberWithPowerOf2(int n)
+{
+    int sum=0;
+    int i=1;
+    while(i*i<=n)
+    {
+        if(n%i==0)
+        {
+            sum+=i;
+            if(i*i!=n)
+                sum+=n/i;
+        }
+        ++i;
+    }
+    return sum-n==n;
+}
+
 int main()
 {
-    int n=27;
-    std::cout<<n<<" is a perfect number? "<<isPerfectNumber(n)<<"\n";
+    int n=28;
+    std::cout<<n<<" is a perfect number? "<<isPerfectNumberWithPowerOf2(n)<<"\n";
     return 0;
 }
