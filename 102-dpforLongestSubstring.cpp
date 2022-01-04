66830e40b05d1297c0dbf0a6496fd8bfae0211f4
Chunbo Liu <cbliu@12sigma.ai>
2022-01-04 21:53:17 +0800

    0104 added the dp for longest substring

---
+++ 102-dpforLongestSubstring.cpp
@@ -0,0 +1,49 @@
+//dp for longest substring
+
+#include<iostream>
+#include<string>
+#include<vector>
+int longestSubString(const std::string& s,int from,int end)  //top2bottom
+{
+    if(from>end)
+        return 0;
+    if(from==end)
+        return 1;
+
+    if(s[from]==s[end])
+        return longestSubString(s,from+1,end-1)+2;
+    return std::max(longestSubString(s,from,end-1),longestSubString(s,from+1,end));
+}
+
+int getLongestSubString(const std::string& s)
+{
+    int sz=s.size();
+    if(sz==0)
+        return 0;
+    if(sz==1)
+        return 1;
+    
+    std::vector<std::vector<int>> dp(sz,std::vector<int>(sz,0));
+
+    for(int i=sz-1;i>=0;--i)
+    {
+        dp[i][i]=1;
+        for(int j=i+1;j<sz;++j)
+        {
+            if(s[i]==s[j])
+                dp[i][j]=dp[i+1][j-1]+2;
+            else
+                dp[i][j]=std::max(dp[i][j-1],dp[i+1][j]);
+        }
+    }
+    return dp[0][sz-1];
+
+}
+
+int main()
+{
+    std::string s="abbbdbb";
+    int res=getLongestSubString(s);
+    std::cout<<res<<'\n';
+    return 0;
+}
