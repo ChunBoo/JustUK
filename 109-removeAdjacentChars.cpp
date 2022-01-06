7920cfd7d1b8eb8ead1d036c28b4c5fa9a27fa5b
cbliu <chunboo.liu@gmail.com>
2022-01-06 09:57:15 +0800

    20220106 109-used stack to remove the adjacent char from a string

---
+++ 109-removeAdjacentChars.cpp
@@ -0,0 +1,38 @@
+//using stack to remove the adjacent chars from a string
+#include<string>
+#include<stack>
+#include<iostream>
+#include<vector>
+
+std::string removeAdjacentCharsFromString(const std::string& s)
+{
+    std::string ret{};
+    int sz=s.length();
+    if(sz==0||sz==1)
+        return s;
+    
+    std::stack<char> st{};
+    for(int i=0;i<sz;++i)
+    {
+        if(!st.empty()&&st.top()==s[i])
+            st.pop();
+        else    
+            st.push(s[i]);
+    }
+
+    for(int i=st.size()-1;i>=0;--i)
+    {
+        ret[i]=st.top();
+        st.pop();
+    }
+    return ret;
+}
+
+
+int main()
+{
+    std::string s{"abcba"};
+    std::string ret=removeAdjacentCharsFromString(s);
+    std::cout<<"After removing: "<<ret<<"\n";
+    return 0;
+}
