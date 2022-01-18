c9270a0fdc5410986eb3381bb87513814a5c67db
cbliu <chunboo.liu@gmail.com>
2022-01-18 10:32:59 +0800

    0118 added the DFS for uniqueBST

--- 117-uniBST.cpp
+++ 117-uniBST.cpp
@@ -1,5 +1,5 @@
 //check the BST is a unique tree or not?
-
+//used the BFS: Bread First Search
 //   1
 // 1    1
 //1 1  1  1
@@ -8,13 +8,15 @@
 #include<iostream>
 #include<set>
 #include<deque>
-bool isUniTree(TreeNode* root)
+
+bool isUniTreeBFS(TreeNode* root)
 {
     if(!root)
         return true;
-    std::deque<TreeNode*> q;
+    std::deque<TreeNode*> q;  //used the deque for BFS
     q.push_back(root);
     std::set<int> s{};
+
     while(!q.empty())
     {
         TreeNode* tempNode=q.front();
@@ -30,12 +32,30 @@ bool isUniTree(TreeNode* root)
     return true;
 }
 
+void dfs(TreeNode* root, std::set<int>& d)
+{
+    if(!root)
+        return ;
+    d.insert(root->m_val);
+    dfs(root->left,d);  // DFS likes recursion
+    dfs(root->right,d);
+}
+
+bool isUniTreeDFS(TreeNode* root)
+{
+    if(!root)
+        return true;
+    std::set<int> d{};
+    dfs(root,d);
+    return d.size()==1;
+}
+
 int main()
 {
     TreeNode* root=new TreeNode(1);
     root->addLeftChild(1);
-    root->addRightChild(2);
-    bool res=isUniTree(root);
+    root->addRightChild(1);
+    bool res=isUniTreeDFS(root);
     std::cout<<"Tree is Unique Value is: "<<res<<'\n';
     return 0;
 }
