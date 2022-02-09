ba4ede25cdade8d651d6b48628c9709200368cfa
cbliu <chunboo.liu@gmail.com>
2022-02-09 10:01:10 +0800

    0209 added the review codes

--- 143-nthUglyNumber.cpp
+++ 143-nthUglyNumber.cpp
@@ -1,37 +1,76 @@
 // #include<heap>
-#include<algorithm>
+#include<algorithm>  //std::make_heap,std::pop_heap,std::push_heap
 #include<iostream>
 #include<vector>
-#include<set>
+#include<set>  // for std::set()
+bool isUglyNumber(int n)
+{
+    // if(n<1)
+    //     return false;
+    if(n==1)
+        return true;
+    std::vector<int> val{2,3,5};
+    for(auto i: val)
+    {
+        while(n%i==0)   //this step is important !!
+        {
+            n/=i;
+        }
+    }
+
+    return n==1;   //if n==1 then it is a ugly number,otherwise it is not.
+}
+int NthUglyNumber(int n)
+{
+    int ans=1;
+    while(true)
+    {
+ 
+        if(isUglyNumber(ans))
+        {
+            n-=1;
+            if(n==0)
+                return ans;
+        }       
+        ans+=1;
+    }
+}
+
+
 int getNthUglyNumber(int n)
 {
-    std::vector<int> min{2,3,5};
+    std::set<int> s{2,3,5};
+    std::vector<int> factors{2,3,5};
+    std::vector<int> temp{2,3,5};
+    // temp.push_back(1);
+    std::make_heap(temp.begin(),temp.end(),std::greater<int>());
 
-    std::vector<int> primerNumber{2,3,5};
-    std::set<int> s{1};
-    int cur=0;
-    for(int i=1;i<n;++i)
-    {    std::make_heap(min.begin(),min.end(),std::greater<int>());
-        cur=min.front();
-        std::pop_heap(min.begin(),min.end(),std::greater<int>());
-        min.pop_back();
-        for(auto v:primerNumber)
+    int ans=0;
+    while(n>1)
+    {
+        int cur=temp.front();
+        ans=cur;
+        std::pop_heap(temp.begin(),temp.end(),std::greater<int>());
+        temp.pop_back();
+        for(auto v:factors)
         {
-            int newValue=cur*v;
+            int newValue=v*cur;
             if(s.find(newValue)==s.end())
             {
                 s.insert(newValue);
-                min.push_back(newValue);
-                std::push_heap(min.begin(),min.end(),std::greater<int>());
+                temp.push_back(newValue);
+                std::push_heap(temp.begin(),temp.end(),std::greater<int>());
             }
-        }  
+        }
+        n-=1;
     }
-    return cur;
+    return ans;
 }
 
 int main()
 {
     int res=getNthUglyNumber(7);
+    // int res=NthUglyNumber(7);
     std::cout<<res<<'\n';
     return 0;
 }
--- 144-splitMaxProduct.cpp
+++ 144-splitMaxProduct.cpp
@@ -1,55 +1,56 @@
 #include<iostream>
 #include<cmath>
-#include<vector>
-int dpForSplitMaxProduct2(int n)//top2bottom
-{
-    std::vector<int> nb(n+1,1);
-    for(int i=1;i<n+1;++i)
-    {
-        for(int j=1;j<i;++j)
-        {
-            int temp=std::max(j*(i-j),j*nb[i-j]);
-            nb[i]=std::max(temp,nb[i]);
-        }
-    }
-    return nb[n];
-}
+#include<vector>  //for the nb
 
-int dpForSplitMaxProduct(int n)//top2bottom
+int greedyForSplitMaxProduct(int n)
 {
-    if(n<=2)
-        return 1;
+    //used the mathematics method to reduce the loops
+    if(n==1)
+        return 0;
+    if(n<3)
+        return 1;   //1==1
     if(n==3)
-        return 2;
-    int ans=1;
-    for(int i=1;i<=n;++i)
-    {
-        int temp=std::max(i*(n-i),i*dpForSplitMaxProduct(n-i));  //O(N*N)
-        ans=std::max(temp,ans);
-    }
-    return ans;
-}
-
-
-int splitMaxProduct(int n) //greedy
-{
-    if(n<=2)
-        return 1;
-    if(n==3)
-        return 2;
-    
+        return 2;   //3==1+2
     int ans=1;
     while(n>4)
     {
         ans*=3;
         n-=3;
     }
-    return ans*n;
-
+    return ans*n;   // why we needs this step?? 
 }
+
+int b2tDPForSplitMaxProduct(int n) //bottom 2 top DP,we need the nb to save the previous values
+{
+    //
+    std::vector<int> dp(n+1,1);
+    for(int i=1;i<n+1;++i)
+    {
+        for(int j=1;j<i;++j)
+        {
+            int temp=std::max(j*(i-j),j*dp[i-j]);
+            dp[i]=std::max(temp,dp[i]);
+        }
+    }
+    return dp[n];
+}
+
+int t2bDPForSplitMaxProduct(int n)
+{
+    int ans=0;
+    for(int i=1;i<n+1;++i)
+    {    
+        int temp=std::max(i*(n-i),i*t2bDPForSplitMaxProduct(n-i));
+        ans=std::max(ans,temp);
+    }
+    return ans;
+}
+
 
 int main()
 {
-    std::cout<<dpForSplitMaxProduct2(5)<<'\n';
+    std::cout<<greedyForSplitMaxProduct(7)<<'\n';
+    std::cout<<b2tDPForSplitMaxProduct(7)<<"\n";
+    std::cout<<t2bDPForSplitMaxProduct(7)<<"\n";
     return 0;
 }
