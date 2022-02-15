e142c3d0d7097b6b5d6fcb28e58811472b3aa9a2
Chunbo Liu <cbliu@12sigma.ai>
2022-02-15 23:05:00 +0800

    0215 added the bottom2top method for 151

--- 151-dpForKnapsack.cpp
+++ 151-dpForKnapsack.cpp
@@ -3,6 +3,29 @@
 #include<iostream>
 #include<vector>
 
+int getMethod2(const std::vector<int>& nums,int Target)   //bottom2top
+{
+    if(Target<0)
+        return 0;
+    if(Target==0)
+        return 1;
+    std::vector<int> dp(Target+1,0);
+    dp[0]=1;   //this step is important!!!
+    for(int i=1;i<Target+1;++i)
+    {
+        for(auto j:nums)
+        {
+            if(i>=j)
+            {
+                dp[i]+=dp[i-j];
+            }
+        }
+    }
+    return dp[Target];
+     
+}
+
+
 int getMethods(const std::vector<int>& nums,int n,std::vector<int>& r)  //top2bottom
 {
     int ans=0;
@@ -28,12 +51,13 @@ int getMethods(const std::vector<int>& nums,int n,std::vector<int>& r)  //top2bo
 
 int main()
 {
-    std::vector<int> nums{1,2,3,4};
+    std::vector<int> nums{1,2,3};
     std::vector<int> rVector{};
     int target=4;
     
     int res=0;
-    res=getMethods(nums,target,rVector);
+    // res=getMethods(nums,target,rVector);
+    res=getMethod2(nums,target);
     std::cout<<res<<'\n';
     for(auto i:rVector)
     {
