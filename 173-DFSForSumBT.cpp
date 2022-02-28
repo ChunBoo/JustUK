0358b09d2b05d2781c4bcf1fd5f0bddbcb667db7
Chunbo Liu <cbliu@12sigma.ai>
2022-02-28 23:04:11 +0800

    0228-173 DFS for SUMBT

---
+++ 173-DFSForSumBT.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include"treeNode.h"
+
+
+bool dfs(TreeNode* root)
+{
+
+    if(!root)
+        return true;
+    int sum=0;
+     bool ans=true;
+    if(root->left)
+    {
+        sum+=root->left->m_val;
+        ans=ans&&dfs(root->left);
+    }
+    if(root->right)
+    {
+        sum+=root->right->m_val;
+        ans=ans&&dfs(root->right);
+    }
+    if(root->left||root->right)
+        if(sum!=root->m_val)    
+            ans=false;
+    return ans;
+}
+
+int main()
+{
+            TreeNode* r1=new TreeNode(5);
+    TreeNode* l1=new TreeNode(3);
+    TreeNode* r11=new TreeNode(2);
+    r1->left=l1;
+    r1->right=r11;
+    std::cout<<dfs(r1)<<'\n';
+    return 0;
+}
