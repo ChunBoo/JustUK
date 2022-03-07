8a8caeff31875d343a75d73bba86791f70333980
cbliu <chunboo.liu@gmail.com>
2022-03-07 10:13:56 +0800

    0307 review the codes of 184

--- 184-dpForMinimumChanges.cpp
+++ 184-dpForMinimumChanges.cpp
@@ -1,50 +1,51 @@
 //given a list of coins and one target,return the minimum changes
-
+//DP have two methods: top2Down and bottom2top
+//bottom2top will calculate each value from 0,so should create one vector to record each elements
+//top2Down will use recursion method 
 #include<iostream>
 #include<vector>
 #include<cmath>
 
-
-
-
-int dpTop2Down(const std::vector<int>& coins, int target)
+double  dpTop2Down(const std::vector<int>& coins, int target)
 {
-    if(target==0)
-        return 0;
-    int ans=100;
-    for(auto c:coins)
-    {
-        int x=100;
-        if(c<=target)
-        {
-            x=dpTop2Down(coins,target-c);
-            if(x!=100)
-                ans=std::min(ans,x+1);
+   if(target==0)
+    return 0;
 
-        }
-    }
+   double ans=INFINITY;
+   for(auto c:coins)
+   {
+       double x = INFINITY;
+       if(target>=c)
+       {
+           x=dpTop2Down(coins,target-c);
+           if(x!=INFINITY)
+             ans=std::min(x+1,ans);   //if used std::min, then the initialized value should be the maximum values
+       }
+   }
     return ans;
 }
 
 
 
-// int dp()
+
 int dpBottom2Top(const std::vector<int>& coins,int target)
 {
-    std::vector<int> dp(target+1,100);
+    if(target==0)
+        return 0;
+    
+    std::vector<float> dp(target+1,INFINITY);
     dp[0]=0;
     for(int i=1;i<target+1;++i)
     {
         for(auto c:coins)
         {
-            if(i>=c&&dp[i-c]!=-1)
+            if(i>=c && dp[i-c]!=INFINITY)
             {
                 dp[i]=std::min(dp[i-c]+1,dp[i]);
             }
-
         }
     }
-    return dp[target];
+    return static_cast<int>(dp[target]);
 }
 
 
@@ -52,5 +53,6 @@ int main()
 {
     std::vector<int> coins{1,5,20,25};
     std::cout<<dpTop2Down(coins,60)<<'\n';
+    // std::cout<<"Result is:"<<dpBottom2Top(coins,60)<<'\n';
     return 0;
 }
