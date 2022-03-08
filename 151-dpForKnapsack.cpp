5eea77b65f3f817e51c1af793376f60f72af951c
cbliu <chunboo.liu@gmail.com>
2022-03-08 10:10:10 +0800

    0308 review the 151 issue

--- 151-dpForKnapsack.cpp
+++ 151-dpForKnapsack.cpp
@@ -1,52 +1,45 @@
 //unbounded knapsack issue,used dp
+//无限背包问题，DP解决
+// given  a list of each weights and the target for the object, return how many methods can be implemented
 
 #include<iostream>
 #include<vector>
 
 int getMethod2(const std::vector<int>& nums,int Target)   //bottom2top
 {
-    if(Target<0)
-        return 0;
     if(Target==0)
         return 1;
+
     std::vector<int> dp(Target+1,0);
-    dp[0]=1;   //this step is important!!!
+    dp[0]=1;
     for(int i=1;i<Target+1;++i)
     {
-        for(auto j:nums)
+        for(auto v:nums)
         {
-            if(i>=j)
+            if(i>=v)
             {
-                dp[i]+=dp[i-j];
+                dp[i]+=dp[i-v];
             }
         }
     }
     return dp[Target];
-     
 }
 
 
-int getMethods(const std::vector<int>& nums,int n,std::vector<int>& r)  //top2bottom
+int getMethods(const std::vector<int>& nums,int target)  //top2bottom
 {
-    std::cout<<"n= "<<n<<'\n';
     int ans=0;
-    // if(n<0)
-    //     return 0;
-    if(n==0)
+    if(target==0)
+        ans+=1;
+    
+    for(auto v:nums)
     {
-        r.push_back(9);
-        return 1;
+        if(target>=v)
+        {
+            ans+=getMethods(nums,target-v);
+        }
     }
-
-    for(auto i:nums)
-    {   
-        // if(i>2)
-        //     break;
-        r.push_back(i);
-        if(i<=n)
-            ans+=getMethods(nums,n-i,r);
-    }
-    return ans;  
+    return ans;
 }
 
 
@@ -58,8 +51,8 @@ int main()
     int target=4;
     
     int res=0;
-    res=getMethods(nums,target,rVector);
-    // res=getMethod2(nums,target);
+    // res=getMethods(nums,target);
+    res=getMethod2(nums,target);
     std::cout<<res<<'\n';
     for(auto i:rVector)
     {
