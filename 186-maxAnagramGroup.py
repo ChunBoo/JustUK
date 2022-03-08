9ea56940501d8ef8ba394337a2ee1827fc77d5e2
Chunbo Liu <cbliu@12sigma.ai>
2022-03-08 22:32:28 +0800

    0308 added 186

---
+++ 186-maxAnagramGroup.py
@@ -0,0 +1,14 @@
+from typing import DefaultDict
+
+
+def maxAnagramGroup(words):
+    nb=DefaultDict(int)
+    for w in words:
+            sorted(w)
+            nb[w]+=1
+    print(nb)
+    return max(nb.values())
+
+
+words=["abc","acb","ab","bcd"]
+print(maxAnagramGroup(words))
