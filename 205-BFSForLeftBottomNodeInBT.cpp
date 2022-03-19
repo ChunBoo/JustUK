2b240231319d6b56aa63dc9541fc5c2c9aa27503
Chunbo Liu <cbliu@12sigma.ai>
2022-03-19 22:52:53 +0800

    0319 updated205

--- 205-BFSForLeftBottomNodeInBT.cpp
+++ 205-BFSForLeftBottomNodeInBT.cpp
@@ -4,7 +4,42 @@
 #include<deque>
 #include<iostream>
 
-int getLeftBottomNodeInBT(TreeNode* root)
+using DT=std::deque<TreeNode*>;
+TreeNode* getLeftBottomNodeInBT(TreeNode* root)
 {
-    
+    if(!root)
+        return nullptr;
+
+    DT q{};
+    q.push_back(root);
+    TreeNode* ans=nullptr;
+    while(!q.empty())
+    {
+        int sz=q.size();
+
+        ans=q.front();
+        // q.pop_front();
+        for(int i=0;i<sz;++i)
+        {
+            TreeNode* curNode=q.front();
+            q.pop_front();
+        if(curNode->left)
+            q.push_back(curNode->left);
+        if(curNode->right)
+            q.push_back(curNode->right);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    TreeNode* root=new TreeNode(2);
+    TreeNode* left=new TreeNode(1);
+    left->addLeftChild(99);
+    left->addRightChild(88);
+    root->addRightChild(3);
+    root->left=left;
+    std::cout<<getLeftBottomNodeInBT(root)->m_val;
+    return 0;
 }
