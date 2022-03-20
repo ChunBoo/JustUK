55aa42103967fdbd328b70859bec14e0acf5ac90
Chunbo Liu <cbliu@12sigma.ai>
2022-03-20 23:23:50 +0800

    0320 added207

---
+++ 207-dfsForIslands.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+
+using DT=std::vector<std::vector<int>>;
+
+int dfs(DT& M,int r,int c,int rows,int cols)
+{
+    if(r<0||r>=rows||c<0||c>=cols)
+        return 0;
+    
+    if(M[r][c]==0)
+        return 0;
+    
+    M[r][c]=0;
+    dfs(M,r-1,c,rows,cols);
+    dfs(M,r+1,c,rows,cols);
+    dfs(M,r,c-1,rows,cols);
+    dfs(M,r,c+1,rows,cols);
+    return 1;
+}
+
+int getIslands(DT& M)
+{
+    if(M.empty())
+        return 0;
+    int R=M.size(),C=M[0].size();
+    int ans=0;
+    for(int r=0;r<R;++r)
+    {
+        for(int c=0;c<C;++c)
+        {
+            ans+=dfs(M,r,c,R,C);
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    DT M={{1,1,0,0},\
+        {0,1,0,0},\
+        {0,0,1,1},\
+        {1,0,0,0}};
+    std::cout<<getIslands(M);
+    return 0;
+}
