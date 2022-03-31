d4a4c262c1f4b904c8eb58735cfc2ea3eafdbdd5
cbliu <chunboo.liu@gmail.com>
2022-03-31 09:56:24 +0800

    0330 added the 220 array/stack method

--- 220-checkPanlindromeForLinkedList.cpp
+++ 220-checkPanlindromeForLinkedList.cpp
@@ -1,22 +1,66 @@
 #include"linkNode.h"
-
+#include<stack>
 #include<iostream>
-
-bool isPanlindromeLinkedList(LinkNode* head)
+#include<vector>
+bool isPanlindromeLinkedList(LinkNode* head)  //used array method
 {
     if(!head)
         return true;
+    std::vector<int> vec{};
+    LinkNode* p=head;
+    while(p)
+    {
+        vec.push_back(p->m_val);
+        p=p->next;
+    }
+    int sz=vec.size();
+    for(int i=0;i<sz/2;++i)
+    {
+        if(vec[i]!=vec[sz-i-1])
+            return false;
+    }
+    return true;
+}
+
+bool isPanlindromeLinkedList3(LinkNode* head)  //used stack method
+{
+    if(!head)
+        return true;
+    std::stack<LinkNode*> s{};
+    LinkNode* p=head;
+    while(p)
+    {
+        s.push(p);
+        p=p->next;
+    }
+    while(head)
+    {
+        LinkNode* topNode=s.top();
+        s.pop();
+        if(topNode->m_val!=head->m_val)
+            return false;
+        head=head->next;
+    }
+    return true;
+}
+bool isPanlindromeLinkedList22(LinkNode* head)  //revert second half of LL method
+{
+    if(!head)
+        return true;
+    //fast,slow pointers method
     LinkNode* fast=head;
     LinkNode* slow=head;
+
     while(fast&&fast->next)
     {
         fast=fast->next->next;
         slow=slow->next;
     }
+    //revert the second half
     LinkNode* right=nullptr;
     while(slow)
     {
-        LinkNode* p=slow->next;
+        LinkNode* p=slow->next;  //create temp pointer to save the slow->next
         slow->next=right;
         right=slow;
         slow=p;
@@ -38,7 +82,7 @@ int main()
     LinkNode* head=new LinkNode(1);
     LinkNode* first=new LinkNode(2);
     LinkNode* second=new LinkNode(2);
-    LinkNode* last=new LinkNode(3);
+    LinkNode* last=new LinkNode(1);
     head->next=first;
     first->next=second;
     second->next=last;
