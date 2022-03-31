ae8588f3c45df2d20ad74b5670d1a38d4af09f31
cbliu <chunboo.liu@gmail.com>
2022-03-31 09:42:23 +0800

    0331 added the review codes of 219

--- 219-dpmethodForMaxSumOfInadjacentNodes.py
+++ 219-dpmethodForMaxSumOfInadjacentNodes.py
@@ -9,24 +9,23 @@ from treeNode import TreeNode
 
 def dp(root):
     if root is None:
-        return 0,0
-    takeLeft,notTakeLeft=dp(root.left)
-    takeRight,notTakeRight=dp(root.right)
-    
+        return (0,0)
+    takeleft,notTakeLeft=dp(root.left)
+    takeright,notTakeRight=dp(root.right)
     takeRoot=root.val+notTakeLeft+notTakeRight
-    notTakeRoot=max(takeLeft,notTakeLeft)+max(takeRight,notTakeRight)
-    
-    return takeRoot,notTakeRoot
+    notTakeRoot=max(takeleft,notTakeLeft)+max(takeright,notTakeRight)
+    return takeRoot,notTakeRoot  #can not return max(takeRoot,notTakeRoot),TypeError: cannot unpack non-iterable int object
 
-root=TreeNode(1);
+
+root=TreeNode(1)
 left=TreeNode(2)
 right=TreeNode(3)
 leftLeft=TreeNode(5)
 leftRight=TreeNode(6)
-left.addLeftChild(leftLeft)
-left.addRightChild(leftRight)
-root.addLeftChild(left)
-root.addRightChild(right)
+left.left=(leftLeft)
+left.right=(leftRight)
+root.left=(left)
+root.right=(right)
 
 res=max(dp(root))
 print(res)
--- 219-maxValuesOfinAdjacentNodes.cpp
+++ 219-maxValuesOfinAdjacentNodes.cpp
@@ -1,19 +1,19 @@
 #include<iostream>
 #include"treeNode.h"
 #include<algorithm>  // can support the list of numbers
-int maxSumOfInadjacentNodes(TreeNode* root,bool toTake)  //recursion method
+int maxSumOfInadjacentNodes(TreeNode* root,bool toTake)  //recursion method top2down
 {
     if(!root)
         return 0;
-    int sum=0,val=0;
+    int ans=0;
     if(toTake)
-        val=root->m_val+maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,false);
-    else
-        val=std::max({maxSumOfInadjacentNodes(root->left,true)+maxSumOfInadjacentNodes(root->right,true),\
-                     maxSumOfInadjacentNodes(root->left,true)+maxSumOfInadjacentNodes(root->right,false),\
-                     maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,true),\
-                     maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,false)});
-    return val;
+        ans=root->m_val+maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,false);
+    else  //not To take,then should pick the max values from it's children
+        ans=std::max({maxSumOfInadjacentNodes(root->left,true)+maxSumOfInadjacentNodes(root->right,true),\
+                      maxSumOfInadjacentNodes(root->left,true)+maxSumOfInadjacentNodes(root->right,false),
+                      maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,true),
+                      maxSumOfInadjacentNodes(root->left,false)+maxSumOfInadjacentNodes(root->right,false)});
+    return ans;
 }
 
 int main()
@@ -21,8 +21,8 @@ int main()
     TreeNode* root=new TreeNode(1);
     TreeNode* left=new TreeNode(2);
     TreeNode* right=new TreeNode(3);
-    left->addLeftChild(4);
-    right->addRightChild(5);
+    left->addLeftChild(5);
+    right->addRightChild(6);
     root->left=left;
     root->right=right;
     int val=std::max(maxSumOfInadjacentNodes(root,true),maxSumOfInadjacentNodes(root,false));
