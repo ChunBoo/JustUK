8541a84f583ce8cb61a78ab5e0e2b38e1425802d
cbliu <chunboo.liu@gmail.com>
2022-04-11 10:16:55 +0800

    0411-added the review codes of 230-231

--- 230-checkNumberIsTimesof3And7.cpp
+++ 230-checkNumberIsTimesof3And7.cpp
@@ -2,30 +2,30 @@
 
 #include<iostream>
 
-bool checkMultiple(int n)
+bool checkMultiple(int n)  //dp method
 {
-    if(n<0)
-      return false;
-    if(n%3==0)
-      return true;
-    if(n%7==0)
-      return true;
-    return checkMultiple(n-3) || checkMultiple(n-7);
+  if(n<0)
+    return false;
+  if(n%3==0)
+    return true;
+  if(n%7==0)
+    return true;
+  return checkMultiple(n-3)||checkMultiple(n-7);
 }
 
 bool f(int n)   //iterator method
 {
-    for(int i=0;i<n/7+1;++i)
-    {
-        if((n-7*i)%3==0)
-            return true;
-    }
-    return false;
+  for(int i=0;i<n/7+1;++i)
+  {
+    if((n-7*i)%3==0)
+      return true;
+  }
+  return false;
 }
 
 int main()
 {
-    std::cout<<checkMultiple(10);
-    std::cout<<'\n'<<f(10);
+    std::cout<<checkMultiple(11);
+    std::cout<<'\n'<<f(11);
     return 0;
 }
--- 231-groupNumbers.cpp
+++ 231-groupNumbers.cpp
@@ -2,7 +2,7 @@
 #include<map>
 #include<iostream>
 #include<vector>
-
+#include<algorithm>
 int mGCD(int a,int b)
 {
     while(b)
@@ -15,24 +15,52 @@ int mGCD(int a,int b)
 }
 bool checkCanBeGroup(std::vector<int>& nums)
 {
+    if(nums.empty())
+        return true;
     std::map<int,int> m{};
     for(auto i:nums)
         m[i]+=1;
-    std::vector<int> group{};
+    std::vector<int> v{};
     for(auto i:m)
-    {
-        group.push_back(i.second);
-    }
-    int ans=group[0];
-    for(auto i:group)
+        v.push_back(i.second);
+    
+    int ans=v[0];
+    for(auto i:v)
     {
         ans=mGCD(ans,i);
     }
     return ans>1;
 }
+
+bool checkCanBeGroup2(std::vector<int>& nums)
+{
+    if(nums.empty())
+        return true;
+    std::map<int,int> m{};
+    for(auto i:nums)
+        m[i]+=1;
+    std::vector<int> v{};
+    for(auto i:m)
+        v.push_back(i.second);
+    
+    int ans=v[0];
+    int minVal=INT_MAX;
+    for(auto i:v)
+    {
+        minVal=std::min(i,minVal);
+    }
+    if(minVal<=1)
+        return false;
+    for(auto i:v)
+    {
+        if((i%minVal)!=0)
+            return false;
+    }
+    return true;
+}
 int main()
 {
     std::vector<int> nums{1,1,2,2,5,5};
-    std::cout<<checkCanBeGroup(nums);
+    std::cout<<checkCanBeGroup2(nums);
     return 0;
 }
