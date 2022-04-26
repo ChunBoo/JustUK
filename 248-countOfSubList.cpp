7c8f8407ea22585dbe7b9f1ad7312a0b8042e2c0
cbliu <chunboo.liu@gmail.com>
2022-04-26 09:46:46 +0800

    0426---added the review codes

--- 248-countOfSubList.cpp
+++ 248-countOfSubList.cpp
@@ -1,7 +1,22 @@
 #include<vector>
 #include<iostream>
 #include<map>
+//return the count of sublist which has some header and tail.
 
+int countWithTwoLoops(std::vector<int>& nums)
+{
+    int sz=nums.size(),ans=0;
+    for(int L=0;L<sz;++L)
+    {
+        for(int R=L;R<sz;++R)
+        {
+            if(nums[L]==nums[R])
+                ans+=1;
+        }
+    }
+    return ans;
+
+}
 int countOfSubList(std::vector<int>& nums)
 {
     int ans=0;
@@ -17,6 +32,6 @@ int countOfSubList(std::vector<int>& nums)
 int main()
 {
     std::vector<int> nums{1,2,3,5,1};
-    std::cout<<countOfSubList(nums);
+    std::cout<<countWithTwoLoops(nums);
     return 0;
 }
