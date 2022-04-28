18c485bdb98daf32bcd8e1761460dff9482809dc
Chunbo Liu <chunboo.liu@gmail.com>
2022-04-28 22:10:22 +0800

    0428-added the changed of 248

--- 248-countOfSubList.cpp
+++ 248-countOfSubList.cpp
@@ -2,6 +2,21 @@
 #include<iostream>
 #include<map>
 
+int count2(std::vector<int>& nums)
+{
+    int sz=nums.size();
+    int ans=0;
+    for(int L=0;L<sz;++L)
+    {
+        for(int R=L;R<sz;++R)
+        {
+            if(nums[L]==nums[R])
+                ans+=1;
+        }
+    }
+    return ans;
+}
+
 int countOfSubList(std::vector<int>& nums)
 {
     int ans=0;
@@ -17,6 +32,6 @@ int countOfSubList(std::vector<int>& nums)
 int main()
 {
     std::vector<int> nums{1,2,3,5,1};
-    std::cout<<countOfSubList(nums);
+    std::cout<<count2(nums);
     return 0;
 }
