eab122fee9a093d885ad866e89f7a501628bb2db
Chunbo Liu <chunboo.liu@gmail.com>
2022-05-02 22:30:16 +0800

    0502 added 256 jump nodes from linklist

---
+++ 256-linkListJump.cpp
@@ -0,0 +1,29 @@
+#include"linkNode.h"
+#include<iostream>
+
+LinkNode* jump(LinkNode* node)
+{
+    if(!node)
+        return nullptr;
+    
+    LinkNode* head=node;
+    for(int i=0;i<node->m_val;++i)
+    {
+        head=head->next;
+        if(!head)
+            break;
+    }
+    node->next=jump(head);
+    return node;
+}
+
+int main()
+{
+    LinkNode* head=new LinkNode(2);
+    head->addNode(2);
+    head->printNode();
+    LinkNode* newHeader=jump(head);
+    // swapLinkNodes(head);
+    newHeader->printNode();
+    return 0;
+}
