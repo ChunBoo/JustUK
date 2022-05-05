bc352acd0fa494b357a90559952732850829d332
cbliu <chunboo.liu@gmail.com>
2022-05-05 09:23:10 +0800

    0505-added the review codes of 259-260

--- 258-isArithmeticSequence.cpp
+++ 258-isArithmeticSequence.cpp
@@ -3,34 +3,33 @@
 #include<vector>
 #include<iostream>
 
-bool isArithmeticSequence2(std::vector<int>& nums)//without set
+bool isArithmeticSequence2(std::vector<int>& nums)//with set
 {
     std::sort(nums.begin(),nums.end());
-    int sz=nums.size();
-    for(int i=1;i<sz;++i)
-    {
-        if((nums[i]-nums[i-1])!=(nums[1]-nums[0]))
-            return false;
-    }
-    return true;
-}
-
-
-bool isArithmeticSequence(std::vector<int>& nums)
-{
-    std::sort(nums.begin(),nums.end());  //O(N*LOGN)
     std::set<int> d{};
     for(int i=1;i<nums.size();++i)
     {
         d.insert(nums[i]-nums[i-1]);
     }
-
     return d.size()==1;
+}
+
+
+bool isArithmeticSequence(std::vector<int>& nums)  //without  set
+{
+    int sz=nums.size();
+    std::sort(nums.begin(),nums.end());
+    for(int i=1;i<sz;++i)
+    {
+        if(nums[i]-nums[i-1]!=nums[1]-nums[0])
+            return false;
+    }
+    return true;
 }
 
 int main()
 {
-    std::vector<int> nums{1,5,7,3};
+    std::vector<int> nums{1,5,7,3,4};
     std::cout<<isArithmeticSequence2(nums);
     return 0;
 }
--- 259-validMaxHeap.cpp
+++ 259-validMaxHeap.cpp
@@ -1,15 +1,16 @@
 #include<vector>
 #include<iostream>
-
+//given a vector of integers,return if those numbers can be a max heap(root is larger than it's children)
 bool isMaxHeap(std::vector<int>& nums)
 {
+    if(nums.empty())
+        return false;
     int sz=nums.size();
-
     for(int i=0;i<sz/2;++i)
     {
-        if(((i*2+1)<sz)&& (nums[i]<nums[i*2+1]))
+        if((i*2+1<sz)&&nums[i]<nums[i*2+1])  //should larger than its left child
             return false;
-        if(((i*2+2)<sz)&& (nums[i]<nums[i*2+2]))
+        if((i*2+2)&&nums[i]<nums[i*2+2])    //should add the ranges check
             return false;
     }
     return true;
@@ -17,7 +18,7 @@ bool isMaxHeap(std::vector<int>& nums)
 
 int main()
 {
-    std::vector<int> nums{100,9,98,97,97,95,94};
+    std::vector<int> nums{100,99,98,97,97,95,94};
     std::cout<<isMaxHeap(nums);
     return 0;
 }
