d2da9995ac84a2075afd99497df08da01bb73b22
cbliu <chunboo.liu@gmail.com>
2022-05-09 09:48:16 +0800

    0509-added review codes of 264-265

--- 264-countAS.py
+++ 264-countAS.py
@@ -1,3 +1,4 @@
+#sliding windows to get the substring in s1
 from collections import Counter
 from itertools import count
 
@@ -5,19 +6,20 @@ def countAS(s0,s1):
     ans=0
     n0=R=len(s0)
     n1=len(s1)
-    c=Counter(s0)
-    cur=Counter(s1[:n0])
-    if cur==c:
-        ans+=1;
-    while R<n1:
+    C,cur=Counter(s0),Counter(s1[:R])    
+    if(cur==C):
+        ans+=1
+    while(R<n1):
         cur[s1[R]]+=1
         cur[s1[R-n0]]-=1
         if(cur[s1[R-n0]]==0):
             del cur[s1[R-n0]]
-        if cur==c:
+        if cur==C:
             ans+=1
         R+=1
-        
     return ans
+    
+        
 
-print(countAS("abc","bacdacb"))
+
+print(countAS("abc","bacbdacb"))
--- 265-prefixSumOfMatrix.cpp
+++ 265-prefixSumOfMatrix.cpp
@@ -1,7 +1,18 @@
+//given a 2-D matrix, which want to return the 
 #include<vector>
 #include<iostream>
 
 using DT=std::vector<std::vector<int>>;
+using DT1=std::vector<int>;
+int getValue(DT1& m,int s,int e)
+{
+    if(s==e)
+        return m[s];
+    int sz=m.size();
+    for(int i=1;i<sz;++i)
+        m[i]+=m[i-1];
+    return m[e]-m[s];
+}
 
 DT prefixSumOfMatrix(DT& m)
 {
@@ -18,15 +29,15 @@ DT prefixSumOfMatrix(DT& m)
     for(int r=1;r<R;++r)
     {
         for(int c=0;c<C;++c)
-        {
             m[r][c]+=m[r-1][c];
-        }
     }
     return m;
 }
 
 int main()
 {
+    DT1 m1{1,2,3,4,5,6};
+    std::cout<<getValue(m1,1,3);
     DT m{{0,1,2},{2,1,4},{3,2,1}};
     DT M=prefixSumOfMatrix(m);
     for(auto r:M)
