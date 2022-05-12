efb6f51d4f06386f18af7911f7a78e4e82b6d284
cbliu <chunboo.liu@gmail.com>
2022-05-12 10:07:42 +0800

    0512- added the review codes of DAG's traversing

--- 267-DFS4SourceToTarget.py
+++ 267-DFS4SourceToTarget.py
@@ -1,16 +1,17 @@
 #given the adjacent list for a DAG,to get all of the path from the source to target
-
+#using the DFS method
 def  allFromSrcToTarget(G):
     ans=[]
-    def dfs(curPath,n):
-        if n==len(G)-1:
+    n=len(G)-1
+    def dfs(curNode,curPath):
+        if curNode==n:
             ans.append(curPath[:])
             return 
-        for nextNode in G[n]:
+        for nextNode in G[curNode]:
             curPath.append(nextNode)
-            dfs(curPath,nextNode)
+            dfs(nextNode,curPath)
             curPath.pop()
-    dfs([0],0)
+    dfs(0,[0])
     return ans
 
 G={0:[1,3],1:[2,3],2:[3],3:[]}
--- 268-BFS4DAGpath.py
+++ 268-BFS4DAGpath.py
@@ -1,21 +1,17 @@
-
-
+#BFS for this issue: using deque 
 from collections import deque
 
-
 def bfs4DAGtaget(G):
-    n=len(G)-1
     ans=[]
+    n=len(G)-1
     q=deque([(0,[0])])
     while q:
         curNode,curPath=q.popleft()
-        if(curNode==n):
+        if n==curNode:
             ans.append(curPath)
         for nextNode in G[curNode]:
-            q.append([nextNode,curPath+[nextNode]])
-    
+            q.append([nextNode,curPath+[nextNode]])   #wrong method: q.append([(nextNode,curPath+[nextNode])])
     return ans
-
 
 G={0:[1,3],1:[2,3],2:[3],3:[]}
 print(bfs4DAGtaget(G))
