bb37002b633707ad0fca2fc302dccbdf24fcf2a9
Chunbo Liu <chunboo.liu@gmail.com>
2022-05-16 22:16:06 +0800

    0516-added shortest distance of Graph

---
+++ 270-floy-warshallForShortestDistance.py
@@ -0,0 +1,23 @@
+
+import math
+def shortestDistance(G,n):
+    dis=[[math.inf for _ in range(n)] for _ in range(n)]
+    for a,b,w in G:
+        dis[a][b]=w
+    
+    for  i in range(n):
+        dis[i][i]=0
+        
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                dis[i][j]=min(dis[i][j],dis[i][k]+dis[k][j])
+                
+    return dis
+
+G=[(1,2,100),(1,3,10),(3,2,20)]
+
+res=shortestDistance(G,4)
+for i in range(3):
+    print(res[i])
+        
