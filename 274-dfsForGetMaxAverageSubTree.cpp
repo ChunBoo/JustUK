67bdccc25eba5230cc8633a76fc53b716e633e10
cbliu <chunboo.liu@gmail.com>
2022-05-17 20:48:43 +0800

    0517-added 274-dfs for max average subtree

---
+++ 274-dfsForGetMaxAverageSubTree.cpp
@@ -0,0 +1,34 @@
+#include"treeNode.h"
+#include<iostream>
+
+#include<cmath>
+using DT=std::pair<int,int>;
+
+DT dfs(TreeNode* root,int& maxVal)
+{
+    if(!root)
+        return DT(0,0);
+    
+    int sumLeft=dfs(root->left,maxVal).first;
+    int countLeft=dfs(root->left,maxVal).second;
+
+    int sumRight=dfs(root->right,maxVal).first;
+    int countRight=dfs(root->right,maxVal).second;
+
+    int S=sumLeft+sumRight+root->m_val;
+    int C=countLeft+countRight+1;
+    maxVal=std::max(S/C,maxVal);
+    return DT(S,C);
+}
+
+
+int main()
+{
+    TreeNode* root=new TreeNode(5);
+    root->addLeftChild(4);
+    root->addRightChild(1);
+    int max=-1;
+    dfs(root,max);
+    std::cout<<max<<'\n';
+    return 0;
+}
