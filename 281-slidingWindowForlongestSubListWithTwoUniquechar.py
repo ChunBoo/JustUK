9f983f24a761e7e0239bf45dde1e3354c2b1b27f
cbliu <chunboo.liu@gmail.com>
2022-05-23 21:51:35 +0800

    0523-added 281-sliding windows for longest substring

---
+++ 281-slidingWindowForlongestSubListWithTwoUniquechar.py
@@ -0,0 +1,18 @@
+from collections import defaultdict
+def longest(s):
+    n=len(s)
+    ans,L,R=0,0,0
+    win=defaultdict(int)
+    while(R<n):
+        win[s[R]]+=1
+        while(L<=R and len(win.keys())>2):
+            win[s[L]]-=1
+            if(win[s[L]]==0):
+                del win[s[L]]
+            L+=1
+        ans=max(ans,R-L+1)
+        R+=1
+    return ans
+
+s="abccbb"
+print(longest(s))
