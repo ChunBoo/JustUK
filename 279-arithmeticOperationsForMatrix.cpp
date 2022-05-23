55490d03dade0f62407423a03eb13011f821471e
cbliu <chunboo.liu@gmail.com>
2022-05-23 08:18:02 +0800

    0523-added the version of matrix multiplication of CPP

---
+++ 279-arithmeticOperationsForMatrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+
+using DT=std::vector<std::vector<int>>;  //多维数组初始化
+
+DT multiplyMatrix(const DT& a, const DT& b)
+{
+    int R=a.size();
+    int C=b[0].size();
+    DT ans(R,std::vector<int>(C,0));  //there is inside std::vector<int>
+    for(int i=0;i<R;++i)
+    {
+        for(int j=0;j<C;++j)
+        {
+            for(int k=0;k<C;++k)
+            {
+                ans[i][j]+=a[i][k]*b[k][j];
+            }
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    DT a{{1,2},{3,4}},b{{1,1},{1,1}};
+    DT ret=multiplyMatrix(a,b);
+    for(auto &d:ret)
+    {
+        std::cout<<'\n';
+        for(auto i:d)
+            std::cout<<i<<',';
+    }
+    return 0;
+}
