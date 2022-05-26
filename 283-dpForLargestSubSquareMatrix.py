42780bfe1a87e9d85d1587decf19f592a49771b1
cbliu <chunboo.liu@gmail.com>
2022-05-26 08:03:38 +0800

    0526-added the reviewing codes

--- 283-dpForLargestSubSquareMatrix.py
+++ 283-dpForLargestSubSquareMatrix.py
@@ -1,3 +1,5 @@
+#needs more time to think again
+
 def largestSubSquareMatrix(M):
     R,C=len(M),len(M[0])
     dp=[[0 for _ in range(C)] for _ in range(R)]
@@ -8,11 +10,12 @@ def largestSubSquareMatrix(M):
             if M[r][c]==0:
                 dp[r][c]=0
             else:
+                dp[r][c]=1
                 if r>0 and c>0 and M[r-1][c]==1 and M[r][c-1]==1 and \
                     M[r-1][c-1]==1:
                         dp[r][c]=min(dp[r-1][c-1],dp[r][c-1],dp[r-1][c])+1;
-            ans=max(ans,dp[r][c]);
-    return ans*ans
+                ans=max(ans,dp[r][c]);
+    return ans**2
 
 m=[[1,1,0,0],\
    [1,1,0,1],\
