6f1a107b37fc43af994660810cd5adaa0a7627b8
Chunbo Liu <chunboo.liu@gmail.com>
2022-05-27 22:34:49 +0800

    0527- evaluate the reverse polish notation

---
+++ 287-evaluateRPN.py
@@ -0,0 +1,19 @@
+def evalRPN(arr):
+    st=[]
+    opers={
+        "+":lambda a,b:a+b,
+        "-":lambda a,b:a-b,
+        "*":lambda a,b:a*b,
+        "/":lambda a,b:a//b,
+    }
+    for e in arr:
+        if e in opers:
+            n2=st.pop()
+            n1=st.pop()
+            st.append(opers[e](int(n1),int(n2)))
+        else:
+            st.append(e)
+    return st.pop()
+
+arr=["1","2","+","3","*"]
+print(evalRPN(arr))
