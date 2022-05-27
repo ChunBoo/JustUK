bd1fd29d777167c21634c881653b35768c6765ac
Chunbo Liu <chunboo.liu@gmail.com>
2022-05-27 22:05:30 +0800

    0527-added 286 get the minimum difference with k numbers from a list

---
+++ 286-minKDiff.py
@@ -0,0 +1,10 @@
+def minKDiff(nums,k):
+    nums.sort()
+    return min(nums[i+k-1]-nums[i] for i in range(len(nums)-k+1))
+
+def minKDiff2(nums,k):
+    nums.sort()
+    return min(nums[i]-nums[i-k+1] for i in range(k-1,len(nums)))
+
+nums=[1,2,3,10,20]
+print(minKDiff2(nums,2))
