835b0f53e9f14ab1ee1605bc4b80a7756830c3d4
Chunbo Liu <chunboo.liu@gmail.com>
2022-06-03 22:00:17 +0800

    0603-added bruteforce method for 294

--- 294-maxArea.py
+++ 294-maxArea.py
@@ -6,6 +6,15 @@ def maxAera(length):
     w=length-2*h
     return h*w
 
-
+def bruteForceMethod(length):
+    maxh=1
+    area=0
+    for h in range(1,length//2):
+        if (length-2*h)*h>area:
+            area=(length-2*h)*h
+            maxh=h
+    return area
+    
 print(maxAera(32)) 
+print(bruteForceMethod(32))
     
