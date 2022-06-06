877e4ed8d72d2f60933edbe8056356cfcc8afeb8
cbliu <chunboo.liu@gmail.com>
2022-06-06 11:27:25 +0800

    0606-added the 299 hash table

--- 299-designHashTable.py
+++ 299-designHashTable.py
@@ -2,42 +2,52 @@ from logging import exception
 
 
 class HashTable:
-    def __init__(self) -> None:
-        self.size=65535
-        self.data=[[] for _ in range(self.size)]
-        
+    def __init__(self):
+        self.size=2
+        self.data=[[] for i in range(self.size)]   
+    
     def getKey(self,akey):
         return akey%self.size
-
-    def get(self,akey):
-        key=self.getKey(akey)
+    
+    def getValue(self,aKey):
+        key=self.getKey(aKey)
         for i in self.data[key]:
             if i[0]==akey:
                 return i[1]
         return -1
-    def update(self,akey,val):  # this key is in list,then update,or append it
+    
+    def update(self,akey,val):
         key=self.getKey(akey)
         for i in self.data[key]:
             if i[0]==akey:
                 i[1]=val
-                return
-        
+                return 
         self.data[key].append((akey,val))
-    
+        
+    def returnData(self):
+        return self.data
+              
     def remove(self,akey):
         key=self.getKey(akey)
         for i in range(len(self.data[key])):
-            if len(self.data[i])>0 and self.data[i][0]==akey:
+            if  self.data[key][i][0]==akey:
                 self.data[key][i]=self.data[key][-1][:]
                 self.data[key].pop()
                 return 
-        raise Exception("Not found the key!")
-                
-                
-                
-
-ht=HashTable()
+        raise  Exception("Not found the key:",akey)
+        
+ht=HashTable()  #here we should add the parenthesis
+for i in range(9):
+    ht.update(i,i)
+'''
 ht.update(1,2)
-print(ht.getKey(1))
-ht.remove(1)
-print(ht.get(1))
+ht.update(2,3)
+ht.update(3,4)
+'''
+# print(ht.getKey(8))
+# # ht.remove(1)
+# print(ht.get(1))
+# print(ht.get(3))
+ht.remove(12)
+curData=ht.returnData()
+print(curData)
