d811f530ef800250b5b8fd809161f77ea6c933a5
cbliu <chunboo.liu@gmail.com>
2022-07-01 07:46:15 +0800

    0701-added the cpp version of 317 runlength

---
+++ 317-runLengthForDecompressing.cpp
@@ -0,0 +1,45 @@
+#include<string>
+// #include<vector>
+#include<map>
+#include<iostream>
+// #include <locale>         // std::locale, std::isdigit
+
+using DT=std::string;
+// using 
+DT runLength(const DT& s)
+{
+    DT ans{};
+    if(s.empty())
+        return ans;
+    // std::locale loc;
+    int c=0;
+    std::map<char,int> data{};
+
+    for(char i:s)
+    {
+        if(i>='0'&&i<='9')
+            c=c*10+i-'0';
+        else
+        {
+            data[i]=c;
+            c=0;
+        }
+    }
+
+    std::map<char,int>::iterator iter=data.begin();
+    for(;iter!=data.end();++iter)
+    {
+        DT tmp(iter->second,iter->first);
+        ans+=tmp;
+    }
+    return ans;    
+}
+
+
+int main()
+{
+    // std::cout<<'1'-'0';
+   DT s{"12a3b4c5d6e"};
+   std::cout<<runLength(s);
+   return 0;
+}
