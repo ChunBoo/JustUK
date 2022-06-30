c8a1bb7e952a4031372a1ebd9c519476791a4a12
cbliu <chunboo.liu@gmail.com>
2022-07-01 07:27:47 +0800

    0701-added the review codes for 317

--- 317-runlengthDecompress.py
+++ 317-runlengthDecompress.py
@@ -1,18 +1,20 @@
+#runLength method for decompressing 
 def  runLength(s):
-    c=0
-    ans=[]
-    
+    if not s:
+        return None
+    data=[]  #for store the [2,"a",3,"b"]
+    c=0      #for count the number
     for i in s:
         if i.isdigit():
-            c=c*10+int(i)
+            c+=c*10+int(i)
         else:
-            ans.append(c)
-            ans.append(i)
+            data.append(c)
+            data.append(i)
             c=0
-            
-    return "".join(ans[i]*ans[i+1] for i in range(0,len(ans),2))
+    
+    return "".join(data[i]*data[i+1] for i in range(0,len(data),2))
 
 
-s="2a3b4c5d6e"
+s="12a3b4c5d6e"
 
 print(runLength(s))
