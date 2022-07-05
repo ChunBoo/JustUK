cc8cc2918488d4ce80fdaef820af47349af85bde
cbliu <chunboo.liu@gmail.com>
2022-07-06 07:55:03 +0800

    0706 added the reviewed codes of 321/322

--- 321-bisect-left-right.py
+++ 321-bisect-left-right.py
@@ -1,7 +1,20 @@
 
 #given one array,likes [1,2,2,3,3,3,4,4,5], bisect_left(array,3) will return the index of 3
 
+#The purpose of Bisect algorithm is to find a position in list where an element needs to be inserted to keep the list sorted.
 
+#Python in its definition provides the bisect algorithms using the module “bisect” which allows to keep the list 
+# in sorted order after insertion of each element. 
+# This is essential as this reduces overhead time required to sort the list again and again 
+# after insertion of each element.
+
+"""
+bisect_left(list, num, beg, end) :- This function returns the position in the sorted list, 
+        where the number passed in argument can be placed so as to maintain the resultant list in sorted order. 
+        If the element is already present in the list, the left most position where element has to be inserted is returned.
+        This function takes 4 arguments, list which has to be worked with, 
+        number to insert, starting position in list to consider, ending position which has to be considered.
+"""
 def bisec_left(arr,x):
     for i in range(len(arr)):
         if arr[i]>=x:
@@ -15,5 +28,34 @@ def bisec_right(arr,x):
         if arr[i]>x:
             return i
     return len(arr)
+
+def bisect_right(a,x,lo=0,hi=None):
+    if lo<0:
+        raise ValueError("Wrong Value of lo\n")
+    if hi is None:
+        hi=len(a)
+    while lo<hi:
+        mid=(lo+hi)//2
+        if a[mid]>x:
+            hi=mid
+        else:
+            lo=mid+1
+    return lo
+
+def bisect_left(a,x,lo=0,hi=None):
+    if lo<0:
+        raise ValueError("Wrong Value of lo.\n")
+    if hi is None:
+        hi=len(a)
+    while lo<hi:
+        mid=(lo+hi)//2
+        if a[mid]>=x:
+            hi=mid
+        else:
+            lo=mid+1
+    return lo
+    
 arr=[1,2,2,3,3,3,4,4,5]
-print(bisec_right(arr, 0))
+# print(bisec_right(arr, 0))
+print(bisect_right(arr, 3,0,6))  #return the right-most pos 
+print(bisect_left(arr, 3,0,6))   #return the left-most position
--- 322-longestSubSequence.py
+++ 322-longestSubSequence.py
@@ -1,3 +1,5 @@
+from bisect import bisect_left
+
 def LIS(arr):
     if not arr:
         return 0
@@ -7,7 +9,20 @@ def LIS(arr):
         for j in range(i):
             if arr[i]>arr[j]:
                 dp[i]=max(dp[i],dp[j]+1)
+    print(dp)
     return max(dp)
 
+
+def greedyLIS(arr):
+    lis=[]
+    for i in arr:
+        x=bisect_left(lis,i)
+        if x==len(lis):  #current val is larger than all elements in lis
+            lis.append(i)
+        else:            #current val is smaller than lis[x]
+            lis[x]=i
+    print(lis)
+    return len(lis)
 arr=[3,2,4,1,7,6,10]
 print(LIS(arr))
+print(greedyLIS(arr))
