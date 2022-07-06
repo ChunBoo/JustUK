9c7d4d198e29b7daebea344a519fb545eab50573
cbliu <chunboo.liu@gmail.com>
2022-07-07 07:45:50 +0800

    0707 added the get count of words for cpp version

---
+++ 323-getTimeofMovingKeys_HashTable.cpp
@@ -0,0 +1,35 @@
+//given a keyboards and one string,to return the total count of keys
+
+#include<string>
+#include<vector>
+#include<iostream>
+#include<map>
+
+using STR=std::string;
+using MAP=std::map<char,int>;
+
+
+int getCount(const STR &keys, const STR &word)
+{
+    int kLen=keys.size(),wLen=word.size();
+    if(!kLen ||!wLen)
+        return 0;
+
+    MAP m{};
+    for(int i=0;i<kLen;++i)
+    {
+        m[keys[i]]=i;
+    }
+
+    int ans=m[word[0]];
+    for(int i=1;i<wLen;++i)
+        ans+=std::abs(m[word[i]]-m[word[i-1]]);
+    return ans;
+}
+
+int main()
+{
+    STR keyboard = "abcdefghijklmnopqrstuvwxyz";
+    STR word = "cba";
+    std::cout<<getCount(keyboard,word);
+}
