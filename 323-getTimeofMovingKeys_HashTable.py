e15f8b79bb714e337d994c0e38b27ba3da4fe7a8
cbliu <chunboo.liu@gmail.com>
2022-07-07 07:27:59 +0800

    0707 added the reviewing codes of 323

--- 323-getTimeofMovingKeys_HashTable.py
+++ 323-getTimeofMovingKeys_HashTable.py
@@ -4,7 +4,7 @@ def getTime(keys,s):
     d={}
     for i,c in enumerate(keys):
         d[c]=i
-    ans=d[keys[0]]
+    ans=d[s[0]]
     
     for i in range(1,len(s)):
         ans+=abs(d[s[i]]-d[s[i-1]])
