828c740f18add86819d5be45fa6c1de93ba88485
cbliu <chunboo.liu@gmail.com>
2022-07-11 08:10:52 +0800

    0711-added the review codes of ranges sum by blocks for CPP version

---
+++ 328-rangesSumByBlocks.cpp
@@ -0,0 +1,56 @@
+//given a array, and the start/end index,to return the sum of this range 
+
+#include<vector>
+#include<iostream>
+#include<cmath>
+using DT=std::vector<int>;
+class Solution
+{
+    private:
+        int m_group{};
+        DT m_blocks{};
+        DT m_nums{};
+    public:    
+        Solution(const DT& nums)
+        {
+            int len=nums.size();
+            m_nums=nums;
+            m_group=static_cast<int>(std::sqrt(len));
+            m_blocks=DT(m_group,0);
+            for(int i=0;i<len;++i)
+                m_blocks[i/m_group]+=nums[i];
+        }
+
+        int rangeOfSum(int start,int end)
+        {
+            
+            if((end<start))
+                return 0;   
+
+            int ans{0};
+            int p=start;
+            while(p<=end)
+            {
+                if((p%m_group==0)&&((p+m_group)<end))
+                {
+                    ans+=m_blocks[p/m_group];
+                    p+=m_group;
+                }
+                else
+                {
+                    ans+=m_nums[p];
+                    p+=1;
+                }
+            }
+            return ans;
+        }
+};
+
+int main()
+{
+    DT nums{1,2,3,4,5,6,7,8,9};
+    Solution s{nums};
+    int res=s.rangeOfSum(1,3);
+    std::cout<<res;
+    return 0;
+}
