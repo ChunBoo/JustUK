92f5bcf7445c1e9a43eeb68f7a88454d7dec33ca
cbliu <chunboo.liu@gmail.com>
2022-07-27 08:18:09 +0800

    0727-added the codes of 343 for CPP version

---
+++ 343-countMaxNodes.cpp
@@ -0,0 +1,58 @@
+#include"treeNode.h"
+#include<iostream>
+#include<cmath>
+
+
+int dfs_postorder(const TreeNode* root,int &res)
+{
+    if(!root)
+        return INT_MIN;
+    
+    int left=0,right=0;
+    if(root->left)
+        left=dfs_postorder(root->left,res);
+    if(root->right)
+        right=dfs_postorder(root->right,res);
+    int tempMax=std::max(left,right);
+    if(root->m_val>=tempMax)
+        res+=1;
+    return std::max(root->m_val,tempMax);
+}
+
+int countOfMaxNodes(const TreeNode* root)
+{
+    if(!root)
+        return 0;
+    int ans=0;
+    dfs_postorder(root,ans);
+    return ans;
+}
+
+/*
+root=TreeNode(6)
+left1=TreeNode(3)
+right1=TreeNode(2)
+left21=TreeNode(6)
+right22=TreeNode(4)
+
+*/
+int main()
+{
+    TreeNode *root=new TreeNode(6);
+    TreeNode *left= new TreeNode(3);
+    TreeNode *right=new TreeNode(2);
+    TreeNode *rightLeft=new TreeNode(6);
+    TreeNode *rightright=new TreeNode(4);
+
+    root->left=left;
+    root->right=right;
+    right->left=rightLeft;
+    right->right=rightright;
+
+    std::cout<<countOfMaxNodes(root);
+    return 0;
+}
+
+
+
+
