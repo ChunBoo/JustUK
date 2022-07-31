d86e04785cb30253f07c11b5c6597478dac559a8
cbliu <chunboo.liu@gmail.com>
2022-07-31 23:03:33 +0800

    0731-added the selection sorting

---
+++ 347-selectSorting.py
@@ -0,0 +1,21 @@
+
+
+
+
+def selectSorting(nums):
+    n=len(nums)
+    
+    for i in range(n):
+        minIdx=i
+        for j in range(i+1,n):
+            if nums[j]<nums[minIdx]:
+                minIdx=j
+        nums[i],nums[minIdx]=nums[minIdx],nums[i]
+      
+    return nums
+
+
+
+nums=[3,5,2,9,1,6];
+newVals=selectSorting(nums)
+print(newVals)
