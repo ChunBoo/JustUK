f3fd54d454b9c1b1bbc1b9567f8e9518b412c7ef
cbliu <chunboo.liu@gmail.com>
2022-08-01 22:32:31 +0800

    0801-added 348 insertion sorting

---
+++ 348-insertionSorting.py
@@ -0,0 +1,19 @@
+
+
+def insertionSorting(A):
+    n=len(A)
+    
+    for i in range(1,n):
+        key=A[i]
+        j=i-1
+        while j>=0 and key<A[j]:
+            A[j+1]=A[j]
+            j-=1
+        A[j+1]=key
+        
+    return A
+
+
+nums=[3,5,2,9,1,6];
+newVals=insertionSorting(nums)
+print(newVals)
