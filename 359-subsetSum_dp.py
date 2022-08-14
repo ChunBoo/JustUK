caf5ad2de7d7d2587a91b9fb5f6e91bab676692b
cbliu <chunboo.liu@gmail.com>
2022-08-14 22:55:41 +0800

    0814-updated the codes of 359/360

--- 359-subsetSum_dp.py
+++ 359-subsetSum_dp.py
@@ -14,7 +14,7 @@ def subSetSum(nums):
         
         return dp(i+1,s-nums[i]) or dp(i+1,s)
     
-    return dp(0,0)
+    return dp(0,total//2)
 
 nums=[1,5,5,11]
 print(subSetSum(nums))
--- 360-subSetSum_dp_bottom2Top.py
+++ 360-subSetSum_dp_bottom2Top.py
@@ -11,7 +11,13 @@ def  subsetSum(nums):
     for i in range(n):
         dp[i][0]=True
         
-    for i in range(n-1,-1,-1):
-        for j in range(half,0,-1):
+    for i in range(n-2,-1,-1):
+        for j in range(half,-1,-1):
             if j<nums[i]:
-                
+                dp[i][j]=dp[i+1][j]
+            else:
+                dp[i][j]=dp[i+1][j] or dp[i+1][j-nums[i]]
+    return dp[0][half]
+
+nums=[1,5,5,11]
+print(subsetSum(nums))
