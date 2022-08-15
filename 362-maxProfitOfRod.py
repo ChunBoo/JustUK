71bbfd4913fadcb2f5698008b53093de1094ecd5
cbliu <chunboo.liu@gmail.com>
2022-08-15 22:56:14 +0800

    0815-added 362 max profit of rods

---
+++ 362-maxProfitOfRod.py
@@ -0,0 +1,18 @@
+
+def maxProfitUnboundedKnapsack(prices,n):
+    
+    # @cache
+    def dfs(n):
+        if n<=0:
+            return 0
+        
+        ans=0
+        for i in range(n):
+            ans=max(ans,dfs(n-i-1)+prices[i])
+        return ans
+    
+    return dfs(n)
+
+
+prices=[1, 3, 5, 7, 7, 7]
+print(maxProfitUnboundedKnapsack(prices, 6))
