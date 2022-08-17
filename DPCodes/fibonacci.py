ff1fa2808f214630cf64da6b82cd328228ad600c
cbliu <chunboo.liu@gmail.com>
2022-08-17 08:08:40 +0800

    0817-review the dp method for fibonacci

---
+++ DPCodes/fibonacci.py
@@ -0,0 +1,27 @@
+
+
+#use hash table to store the intermediate values
+def dp_bottom2Top(n):
+    f0,f1=0,1
+    for _ in range(n):
+        f0,f1=f1,f0+f1
+    return f1
+    
+
+def f_recurssion(n):
+    if n==0 or n==1:
+        return n
+    return f(n-1)+f(n-2)
+
+def f(n,nb={}):  #this is top2bottom method, f(n) should calculate the f(n-1) until f(0)
+    if n==0 or n==1:
+        return n
+    if n in nb:
+        return nb[n]
+    val=f(n-1)+f(n-2)
+    nb[n]=val
+    return val
+
+
+print(f(6))
+print(f_recurssion(6))
