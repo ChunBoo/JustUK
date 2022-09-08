d3c7aaef01aee701b3b4811b82d93b4a7bd9abdc
cbliu <chunboo.liu@gmail.com>
2022-09-08 19:15:33 +0800

    0908-added the updated codes of 369 with new method of accessing map and set

--- graph/369-findIncomingNodes.cpp
+++ graph/369-findIncomingNodes.cpp
@@ -3,40 +3,36 @@
 #include<set>
 #include<vector>
 
-using VEC=std::vector<int>;
+using VEC=std::vector<char>;
 using MAP=std::map<char,char>;
 VEC findEntryOfGraph(MAP& G)
 {
-    std::set<int> s{};
-
-    VEC ans{};
-    std::map<char,int> tmp{};
-    std::map<char,char>::iterator iter=G.begin();
-    for(;iter!=G.end();++iter)
+    std::set<char> seen{};
+    std::map<char,int> counter{};
+    for( auto &[k,v]:G)
     {
-        s.insert(iter->first);
-        s.insert(iter->second);
-        tmp[iter->second]++;
+        seen.insert(k);
+        seen.insert(v);
+        counter[v]+=1;
     }
-    
-    std::set<int>::iterator siter=s.begin();
-    for(;siter!=s.end();++siter)
+    VEC ans{};
+    for(auto &c:seen)
     {
-        if(tmp[*siter]==0)
-            ans.push_back(*siter);
+        if(counter[c]==0)
+            ans.push_back(c);
     }
     return ans;
 }
 
 int main()
 {
-    MAP edges;
-    edges['A']='B';
-    edges['B']='C';
-    edges['D']='C';
+    MAP edges{{'A','B'},{'B','C'},{'D','C'}};
+    // edges['A']='B';
+    // edges['B']='C';
+    // edges['D']='C';
     VEC res=findEntryOfGraph(edges);
-    for(char i:res)
-        std::cout<<i;
+    for(auto i:res)
+        std::cout<<i<<',';
 }
 
 
