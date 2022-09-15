9057ae8824f4ec00fab753bdf45c85ebae403599
cbliu <chunboo.liu@gmail.com>
2022-09-15 08:34:42 +0800

    0915-added the reviewing codes of 373 knapsack bottom 2 top

--- Knapsack/373-zero_oneMaxValuesB2T.cpp
+++ Knapsack/373-zero_oneMaxValuesB2T.cpp
@@ -8,11 +8,29 @@ using MATRIX=std::vector<VEC>;
 int Bag(VEC& w, VEC& v,int c)
 {
     int n=w.size();
-
     MATRIX dp(n,VEC(c+1,0));
 
-    for(int i=0;i<c;++i)
+    //initialize the first row
+
+    for(int i=0;i<c+1;++i)
     {
-        dp[0][i]=
+        dp[0][i]=(i>=w[0])?v[0]:0;
     }
+
+    for(int i=1;i<n;++i)
+    {
+        for(int j=0;j<c+1;++j)
+        {
+            int pick=(j>=w[i])?(dp[i-1][j-w[i]]+v[i]):0;
+            dp[i][j]=std::max(dp[i-1][j],pick);
+        }
+    }
+    return dp[n-1][c];
+}
+
+int main()
+{
+    VEC weights{1,2,3};
+    VEC values{1,5,3};
+    std::cout<<Bag(weights,values,5);
 }
