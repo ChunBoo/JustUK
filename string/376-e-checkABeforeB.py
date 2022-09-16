caeb8289491a59323f4972a4d5860682274fb116
Chunbo Liu <chunboo.liu@gmail.com>
2022-09-16 23:48:51 +0800

    0916 added the codes of 376 itertools.groupby

---
+++ string/376-e-checkABeforeB.py
@@ -0,0 +1,22 @@
+from struct import iter_unpack
+
+
+import itertools
+def check(s):
+    if not s:
+        return True
+    
+    l=[i for i,j in itertools.groupby(s)]
+    
+    if len(l)==1:
+        return True
+    
+    if len(l)>2:
+        return False
+    
+    return l[0]=='a'
+
+
+s="aba"
+
+print(check(s))
