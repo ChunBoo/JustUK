0cc3508cffa47ab26cf1f2a3c01de4c2dc47c25d
Chunbo Liu <chunboo.liu@gmail.com>
2022-09-17 23:28:59 +0800

    0917  added 375 split string as two substring with same distinct chars

---
+++ string/375-e-splitStringAsTwoSubStringWithSameDistinctChars.py
@@ -0,0 +1,57 @@
+
+from collections import Counter
+
+def split2(s):
+    if not s:
+        return 0
+    
+    ans=0
+    
+    for i in range(len(s)):
+        c1=Counter(s[:i+1])
+        c2=Counter(s[i+1:])
+        if len(c1)==len(c2):
+            ans+=1
+            print(s[:i+1],s[i+1:])
+    return ans
+
+
+def split2Faster(s):
+    if not s:
+        return 0
+    
+    ans=0
+    
+    for i in range(len(s)):
+        c1=s[:i+1]
+        c2=s[i+1:]
+        if set(c1)==set(c2):
+            ans+=1
+            print(c1,c2)
+            
+    return ans
+
+def split2Counter(s):
+    if not s:
+        return 0
+    
+    c1=Counter()
+    c2=Counter(s)
+    
+    ans=0
+    
+    for i in range(len(s)-1):
+        c1[s[i]]+=1
+        c2[s[i]]-=1
+        if c2[s[i]]==0:
+            del c2[s[i]]
+        
+        if set(c1.keys())==set(c2.keys()):
+            ans+=1
+            print(c1,c2)
+            
+    return ans    
+    
+print(split2("aabab"))
+print(split2Faster("aabab"))
+print(split2Counter("aabab"))
