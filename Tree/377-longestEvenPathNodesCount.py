92bd8f643ceb4aa2985818a2953b69d82a59b8a0
Chunbo Liu <chunboo.liu@gmail.com>
2022-09-18 23:05:48 +0800

    0918-added the codes of 377

---
+++ Tree/377-longestEvenPathNodesCount.py
@@ -0,0 +1,54 @@
+"""
+Given a binary tree root, return the longest path consisting of even values between any two nodes in the tree.
+"""
+
+
+class Tree:
+    def __init__(self,val,left=None,right=None) -> None:
+        self.val=val
+        self.left=left
+        self.right=right
+        
+    
+
+class Solution:
+    def longestEvenPathNodesCount(self,root):
+        if not root:
+            return 0
+        
+        ans=0
+        
+        def dfs(root):
+            if not root:
+                return 0
+            nonlocal ans
+            l=dfs(root.left)
+            r=dfs(root.right)
+            if  root.val&1==0:
+                ans=max(ans,l+r+1)
+                return 1+max(l,r)
+            return 0
+            
+        dfs(root)
+        return ans
+    
+    
+root=Tree(0)
+left1=Tree(8)
+right1=Tree(2)
+
+rLeft2=Tree(6)
+rright2=Tree(0)
+rLeft3=Tree(4)
+
+root.left=left1
+root.right=right1
+
+right1.left=rLeft2
+right1.right=rright2
+
+rLeft2.left=rLeft3
+
+s=Solution()
+print(s.longestEvenPathNodesCount(root))
+                
