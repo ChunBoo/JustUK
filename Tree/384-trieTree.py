c123948bb48922a454561f94931fc5b55b9d43ca
cbliu <chunboo.liu@gmail.com>
2022-09-27 22:56:31 +0800

    0927 added the codes of 384 trieTree

---
+++ Tree/384-trieTree.py
@@ -0,0 +1,40 @@
+
+
+
+class Trie():
+    def __init__(self):
+        self.words={}
+        self.isWord=False
+    
+    def insert(self,word):
+        root=self
+        for w in word:
+            if w not in root.words:
+                root.words[w]=Trie()
+            root=root.words[w]
+        root.isWord=True
+    
+    
+    def dfs(self,word):
+        root=self
+        for i, w in enumerate(word):
+            if w=="?":
+                for n in root.words:
+                    if dfs(word[i+1:],root.words[n]):
+                        return True
+                return False
+    def search(self,word):
+        root=self
+        for w in word:
+            if w not in root.words:
+                return False
+            root=root.words[w]
+        return root.isWord
+    
+    def searchPrefix(self,word):
+        root=self
+        for w in word:
+            if w not in root.words:
+                return False
+            root=root.words[w]
+        return True
