4902e11f270602a91658efbf42fd4675ee14b27a
cbliu <chunboo.liu@gmail.com>
2022-09-27 08:32:14 +0800

    0927 updated the twopointers method of python version

--- string/383-captiatize.py
+++ string/383-captiatize.py
@@ -7,6 +7,24 @@ def captitize(s):
             words[i]=words[i].title()
     return " ".join(words)
 
+def twoPointers(s):
+    w=list(s)
+    n=len(s)
+    
+    i=0
+    j=0
+    
+    while(i<=n):
+        if i==n or s[i]==' ':
+            if i-j>2:
+                w[j]=w[j].upper();
+            j=i+1;
+        else:
+            w[i]=w[i].lower()
+        i+=1
+            
+    return "".join(w) 
+
 
 s="eRIC IS rYANS GOOD FRIEND"
-print(captitize(s))
+print(twoPointers(s))
