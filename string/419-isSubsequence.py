5f3b5799bee5a9ff975ceded4e6178db833d0a20
Chunbo Liu <chunboo.liu@gmail.com>
2022-10-27 23:15:32 +0800

    1027-added 419 issubsequence

---
+++ string/419-isSubsequence.py
@@ -0,0 +1,27 @@
+
+
+class Solution(object):
+    def isSubsequence(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        if len(s)>len(t):
+            return False
+        i,j=0,0
+        while(i<len(s) and j<len(t)):
+            if s[i]==t[j]:
+                i+=1
+                j+=1
+            j+=1
+    
+        return i==len(s)
+s="acb"
+t="ahbgdc"    
+# s="aaaaaa"
+# t="bbaaaa"    
+# s="abc"
+# t="ahbgdc"
+ss=Solution()
+print(ss.isSubsequence(s,t))
