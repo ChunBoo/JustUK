ab372f384d6a2906a2eb661adee17a612ad4201c
cbliu <chunboo.liu@gmail.com>
2022-11-09 08:58:42 +0800

    1109 added the cpp version of 431-restore ip address

--- DFS_BFS/431-restoreIP.py
+++ DFS_BFS/431-restoreIP.py
@@ -1,24 +1,21 @@
 
 def restoreIP(s):
-    if len(s)>12 or len(s)<4:
+    if len(s)<4 or len(s)>12:
         return []
     ans=[]
-    def dfs(cur):
+    def f(cur): #list of current branches
         n=sum(len(x) for x in cur)
         if len(cur)==4:
             if n==len(s):
                 ans.append(".".join(cur))
-        
             return 
-        
         for i in range(n,min(n+3,len(s))):
-            x=s[n:i+1]
-            if 0<=int(x)<=255 and (x=='0' or (len(x)>0 and x[0]!='0')):
+            x=s[n:i+1]      
+            if 0<=int(x)<=255 and ((x=='0') or (len(x)>0 and x[0]!='0')):
                 cur.append(x)
-                dfs(cur)
+                f(cur)
                 cur.pop()
-    
-    dfs([])
+    f([])
     return ans
 
 s="12345"
---
+++ backtracking/431-restoreIP.cpp
@@ -0,0 +1,86 @@
+/*
+valid IP address consists of exactly four integers separated by single dots. Each integer is between 0 and 255 (inclusive) and cannot have leading zeros.
+
+For example, “0.1.2.201” and “192.168.1.1” are valid IP addresses, but “0.011.255.245”, “192.168.1.312” and “192.168@1.1” are invalid IP addresses.
+Given a string s containing only digits, return all possible valid IP addresses that can be formed by inserting dots into s. You are not allowed to reorder or remove any digits in s. You may return the valid IP addresses in any order.
+
+Example 1:
+Input: s = “25525511135”
+Output: [“255.255.11.135″,”255.255.111.35”]
+
+Example 2:
+Input: s = “0000”
+Output: [“0.0.0.0”]
+
+Example 3:
+Input: s = “101023”
+Output: [“1.0.10.23″,”1.0.102.3″,”10.1.0.23″,”10.10.2.3″,”101.0.2.3”]
+
+Constraints:
+1 <= s.length <= 20
+s consists of digits only.
+*/
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+using STR=std::string;
+using VEC=std::vector<STR>;
+
+template <typename Iter>
+std::string join(Iter begin, Iter end, std::string const& separator)
+{
+  std::ostringstream result;
+  if (begin != end)
+    result << *begin++;
+  while (begin != end)
+    result << separator << *begin++;
+  return result.str();
+}
+
+void f(VEC& cur, VEC& ans,STR& s)
+{
+    int _sum=0;
+    for(auto &v:cur)
+        _sum+=v.size();
+    
+    if(cur.size()==4)
+    {
+        if(_sum==s.size())
+            ans.push_back(join(cur.begin(),cur.end(),"."));
+        return;
+    }
+    for(int i=_sum;i<std::min(_sum+3,(int)s.size());++i))
+    {
+        STR x=s.substr(_sum,i-_sum);
+        int tmp=std::stoi(x);
+        if( (tmp>=0 && tmp<=255 ) &&(x=="0" || (x.size()>0 && x[0]!='0')))
+        {
+            cur.push_back(x);
+            f(cur,ans,s);
+            cur.pop();
+        }
+    }
+}
+
+VEC restoreAddress(STR& s)
+{
+    VEC ans{};
+    VEC cur{};
+    int sz=s.size();
+    if(sz<4 || sz>12)
+        return ans;
+    
+    f(cur,ans,s);
+    return ans;
+}
+
+int main()
+{
+    STR s{"1234"};
+    VEC res=restoreAddress(s);
+    for(auto &v:res)
+    {
+        std::cout<<v<<',';
+    }
+}
