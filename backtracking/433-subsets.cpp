ff66a43a17c2b2c3ee516817ef2d7d97851f837c
cbliu <chunboo.liu@gmail.com>
2022-11-11 08:32:35 +0800

    20221111-updated the cpp codes of subsets

--- backtracking/433-subsets.cpp
+++ backtracking/433-subsets.cpp
@@ -9,22 +9,23 @@ void f(const VEC& nums, MATRIX&  res,VEC& cur,int i)
     int n=nums.size();
     if(i==n)
     {
-        VEC tmp=VEC{cur.begin(),cur.end()};
-        res.push_back(tmp);
-        return ;  //this line can not be ignored
+        res.push_back(cur);
+        return;
     }
-    VEC tmp1=VEC{cur.begin(),cur.end()};
-    f(nums,res,tmp1,i+1);
+    VEC tmp{cur.begin(),cur.end()};
+    f(nums,res,tmp,i+1);
     cur.push_back(nums[i]);
-    VEC tmp2=VEC{cur.begin(),cur.end()};
-
+    VEC tmp2{cur.begin(),cur.end()};
     f(nums,res,tmp2,i+1);
 }
 
 MATRIX subSets(const VEC& nums)
 {
     MATRIX ans{};
-    VEC cur{};
+    int n=nums.size();
+    if(n==0)
+        return ans;
+    VEC cur;
     f(nums,ans,cur,0);
     return ans;
 }
