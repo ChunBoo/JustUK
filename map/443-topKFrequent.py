62ecea192ec8adb531150819b2c8491050d17057
Chunbo Liu <chunboo.liu@gmail.com>
2022-11-19 22:07:59 +0800

    11-19 added the codes of top k numbers

--- map/443-topKFrequent.py
+++ map/443-topKFrequent.py
@@ -1,5 +1,5 @@
 
-
+import heapq
 
 from collections import Counter
 def topKFrequent(nums,k):
@@ -11,7 +11,13 @@ def topKFrequent(nums,k):
 def heapMethod(nums,k):
     c=Counter(nums)
     a=[]
-    for k,v in c.items():
-        heappush(a,(-v,k))        
+    for _k,v in c.items():
+        heapq.heappush(a,(-v,_k))    
+    ans=[]
+    for _ in range(k):
+        _,_v = heapq.heappop(a)
+        ans.append(_v)
+    return ans    
 nums=[1,1,1,2,2,3]
 print(topKFrequent(nums,2))
+print(heapMethod(nums,2))
