e8b74fc3b08e0c7a2e901a8f6abc491f8b27a8a2
Chunbo Liu <chunboo.liu@gmail.com>
2022-12-05 22:58:40 +0800

    1205 added the texteditr

---
+++ stack/460-backspace.py
@@ -0,0 +1,31 @@
+'''
+a string s representing characters typed into an editor, with “<-” representing a backspace, return the current state of the editor.
+
+Constraints
+n ≤ 100,000 where n is the length of s
+Example 1
+Input
+s = “abc<-z”
+Output
+“abz”
+Explanation
+The “c” got deleted by the backspace.
+
+Example 2
+Input
+s = “<-x<-z<-”
+Output
+“”
+'''
+def texteditor(s):
+    ans=[]
+    n=len(s)
+    
+    for i in range(n):
+        if i>0 and s[i]=='-' and s[i-1]=='<':
+            ans.pop()
+            if ans:
+                ans.pop()
+        else:
+            ans.append(s[i])
+    return "".join(ans)
