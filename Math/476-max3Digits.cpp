ded61a8b55c149af4f15ae2379e308b78745a540
Chunbo Liu <chunboo.liu@gmail.com>
2022-12-17 23:27:51 +0800

    1217 added the word breaks

---
+++ Math/476-max3Digits.cpp
@@ -0,0 +1,35 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+/*
+You are given a string num representing a large integer. An integer is good if it meets the following conditions:
+
+It is a substring of num with length 3.
+It consists of only one unique digit.
+Return the maximum good integer as a string or an empty string "" if no such integer exists.
+*/
+//need to check the std::find
+using STR=std::string;
+
+
+STR largestGoodInteger( STR nums)
+{
+    STR ans{};
+    for(int c=9;c>=0;--c)
+    {
+        char i=c+'0';   //char(9)=='\t'
+        STR tmp(3,c+'0');
+        auto isFound=nums.find(tmp);  //find(),return std::string::size_t
+        // auto isFound=std::find(nums.begin(),nums.end(),tmp);// can not work,
+        // as std::find() will return iterator
+        if(isFound!=std::string::npos) //std::string::npos is -1
+            return tmp;
+    }
+    return ans;
+}
+
+int main()
+{
+    STR nums{"6777133339"};
+    std::cout<<largestGoodInteger(nums);
+}
