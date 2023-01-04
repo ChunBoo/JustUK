3dd68c836e143194f0a315d0e48a9d945bfaeaea
Chunbo Liu <chunboo.liu@gmail.com>
2023-01-04 23:07:40 +0800

    0104-added 485 longest substring

---
+++ string/485-lengthOfLongestSubstring.py
@@ -0,0 +1,16 @@
+
+
+def longestSubString(s):
+    n=len(s)
+    l=r=0
+    ans=0
+    win=set()
+    while r<n:
+        while s[r] in win:
+            win.remove(s[l])
+            l+=1
+        win.add(s[r])
+        ans=max(ans,len(win))
+        r+=1
+    return ans
+            
