a9b9a86990e888a4d894073e38163b892d2b92b9
cbliu <chunboo.liu@gmail.com>
2023-01-09 10:34:36 +0800

    0109 added the cpp version of 489 greatest letter

---
+++ string/489-greatestLetter.cpp
@@ -0,0 +1,27 @@
+#include<string>
+#include<iostream>
+#include<algorithm>
+
+using STR=std::string;
+
+STR getGreatestLetter( STR s)
+{
+    // STR tmp=std::sort(s.begin(),s.end());
+    std::sort(s.begin(),s.end());
+    int n=s.size();
+    for(int i=n-1;i>-1;--i)
+    {
+        char upper=std::toupper(s[i]);
+        char lower=std::tolower(s[i]);
+        
+        if(s.find(upper)!=std::string::npos && s.find(lower)!=std::string::npos)
+            return STR(1,upper);
+    }
+    return "EMPTY";
+}
+
+int main()
+{
+    STR s{"AbCdEkfGhIjBK"};
+    std::cout<<getGreatestLetter(s);
+}
