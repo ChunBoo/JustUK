143564619b53a15db49d6819c77606d3e880c0a5
Chunbo Liu <chunboo.liu@gmail.com>
2023-01-21 23:40:53 +0800

    0121-added 501 shortest distance with BFS method

---
+++ graph/501-shortestPath-BFS.py
@@ -0,0 +1,41 @@
+#BFS only works for directed unweighted graph
+from collections import deque
+from math import inf
+def shortestPath(G,paths):
+    n=len(G)
+    d=[[0 if i==j else inf for i in range(n)] for j in range(n)]
+    
+    for i,v in enumerate(G):
+        for j in v:
+            d[i][j]=1
+    def bfs(i,j):
+        if i==j:
+            return 0
+        q=deque([i])
+        d=0
+        seen=set()
+        while q:
+            d+=1
+            n=len(q)
+            for _ in range(n):
+                c=q.popleft()
+                if c==j:
+                    return d-1
+                for x in G[c]:
+                    if x not in seen:
+                        seen.add(x)
+                        q.append(x)
+        return 0
+    return bfs(shipments[0][0],shipments[0][1])
+
+ports = [
+    [2, 3],
+    [2],
+    [1, 0],
+    [4],
+    []
+]
+shipments = [
+    [2, 4]
+]
+print(shortestPath(ports,shipments))
