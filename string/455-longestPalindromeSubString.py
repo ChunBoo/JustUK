5da63ebb8546b762f3c8a9a2a032be5f3b775b71
Chunbo Liu <chunboo.liu@gmail.com>
2023-01-23 23:38:17 +0800

    0123 updated the codes of 455 longest palindrome

--- string/455-longestPalindromeSubString.py
+++ string/455-longestPalindromeSubString.py
@@ -1,14 +1,5 @@
 from functools import lru_cache
 
-@lru_cache
-def f(i,j,s):   #top2bottom
-    if i>j or i>=len(s) or j>=len(s):
-        return 
-    if i==j:
-        return True
-    if i+1==j:
-        return s[i]==s[j]
-    return s[i]==s[j] and f(i+1,j-1,s)
 def longestPalindromeDP(s):  #bottom to up
     n=len(s)
     F=[[True for _ in range(n)] for _ in range(n)]
@@ -26,19 +17,30 @@ def longestPalindromeDP(s):  #bottom to up
     return s[L:R+1]
 def longestPalindrome(s):
     n=len(s)
-    L=R=-1
+    L=R=0
+    
+    @lru_cache
+    def f(i,j):   #top2bottom
+        if i>j :
+            return False
+        if i==j:
+            return True
+        if i+1==j:
+            return s[i]==s[j]
+        return s[i]==s[j] and f(i+1,j-1)
     for i in range(n):
         # for j in range(i+1,n):
         for j in range(n-1,i-1,-1):    
             c=s[i:j+1]
             # if c[::-1]==c:
-            if f(i,j,c):
+            if f(i,j):
                 if j-i>R-L:
                     L=i
                     R=j
                 break
     return s[L:R+1]
 
-s="abbccccbbaef"
-print(longestPalindrome(s))
+# s="abbccccbbaef"
+s="cbbd"
+# print(longestPalindrome(s))
 print(longestPalindromeDP(s))
