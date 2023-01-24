9748b5e90f5e9563d24cd2343403adeebeab2f71
Chunbo Liu <chunboo.liu@gmail.com>
2023-01-24 22:49:07 +0800

    0124-added the 504 shortest distance

---
+++ graph/504-distanceBetweenBusStops.py
@@ -0,0 +1,22 @@
+'''
+A bus has n stops numbered from 0 to n - 1 that form a circle. We know the distance between all pairs of neighboring stops where distance[i] is the distance between the stops number i and (i + 1) % n.
+
+The bus goes along both directions i.e. clockwise and counterclockwise.
+
+Return the shortest distance between the given start and destination stops.
+
+'''
+
+def shortestDistance(distance,start,stop):
+    if start>stop:
+        start,stop=stop,start
+    
+    d1=sum(distance[start:stop])
+    d2=sum(distance)-d1
+    return min(d1,d2)
+
+distance=[11,2,3,4]
+start=0
+stop=1
+
+print(shortestDistance(distance,start,stop))
