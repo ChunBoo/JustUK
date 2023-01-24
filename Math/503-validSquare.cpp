dc07e697f8907e9eef0384ac9a92562551cbfa2d
Chunbo Liu <chunboo.liu@gmail.com>
2023-01-24 20:01:44 +0800

    0124 added the cpp codes of validating square

---
+++ Math/503-validSquare.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<map>
+#include<cmath>
+using VEC=std::vector<int>;
+using MATRIX=std::vector<VEC>;
+using MAP=std::map<double,int>;
+
+double dis(const VEC& a, const VEC& b)
+{
+    return std::sqrt(std::pow(a[0]-b[0],2)+std::pow(a[1]-b[1],2));
+}
+
+bool validSquare(const VEC& p1, const VEC& p2, const VEC& p3, const VEC& p4)
+{
+    MATRIX arr{p1,p2,p3,p4};
+    MAP d{};
+    for(int i=0;i<4;++i)
+    {
+        for(int j=0;j<i;++j)
+        {
+            double dd=dis(arr[i],arr[j]);
+            d[dd]+=1;
+        }
+    }
+    return (d.size()==2) && d[0]==0;
+}
+
+int main()
+{
+    VEC p1 = {0,0};
+    VEC p2 = {1,1};
+    VEC p3 = {1,0};
+    VEC p4 = {0,2};
+    bool res=validSquare(p1,p2,p3,p4);
+    std::cout<<res;
+}
