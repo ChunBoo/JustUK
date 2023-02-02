f144f77d87bb57ac163ee7df7b47d7eab3d999ec
cbliu <chunboo.liu@gmail.com>
2023-02-02 08:52:07 +0800

    0202 updated the codes of 511 with the bst

--- Tree/511-validBST.py
+++ Tree/511-validBST.py
@@ -1,11 +1,12 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+from math import inf
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+    def isValidBST(self, root):
         if not root:
             return True
         # INF=pow(2,31)
@@ -21,16 +22,36 @@ class Solution:
         
         return True
     
-        def isValidBST2(self, root: Optional[TreeNode]) -> bool:   
-            prev = -inf
-            def dfs(root):
-                nonlocal prev
-                if not root:
-                    return True
-                if not dfs(root.left):
-                    return False
-                if root.val <= prev:
-                    return False
-                prev = root.val
-                return dfs(root.right)        
-            return dfs(root) 
+    def isValidBST2(self, root):
+        prev = -inf
+        def dfs(root):
+            nonlocal prev
+            if not root:
+                return True
+            if not dfs(root.left):
+                return False
+            if root.val <= prev:
+                return False
+            prev = root.val
+            return dfs(root.right)        
+        return dfs(root) 
+
+six=TreeNode(9)
+seven=TreeNode(7)
+four=TreeNode(4)
+five=TreeNode(5)
+three=TreeNode(3)
+two=TreeNode(2)
+one=TreeNode(1)
+eight=TreeNode(8)
+
+two.left=one
+two.right=three
+five.left=four
+five.right=six
+four.left=two
+seven.left=five
+
+seven.right=eight
+
+print(Solution().isValidBST2(seven))
