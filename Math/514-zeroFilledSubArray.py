42ef6cf520c37f53bc6ef6876b3e160c2424ae75
Chunbo Liu <chunboo.liu@gmail.com>
2023-02-04 22:05:14 +0800

    0204-added the 514

---
+++ Math/514-zeroFilledSubArray.py
@@ -0,0 +1,37 @@
+'''
+Given an integer array nums, return the number of subarrays filled with 0.
+
+A subarray is a contiguous non-empty sequence of elements within an array.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,0,0,2,0,0,4]
+Output: 6
+Explanation: 
+There are 4 occurrences of [0] as a subarray.
+There are 2 occurrences of [0,0] as a subarray.
+There is no occurrence of a subarray with a size more than 2 filled with 0. Therefore, we return 6.
+
+'''
+from itertools import groupby
+class Solution:
+    def zeroFilledSubarray(self, nums):
+        ans=0
+        for a,b in groupby(nums):
+            if a==0:
+                n=len(list(b))
+                ans+=(n*(n+1))//2
+        return ans
+        # z=0
+        # for i in nums:
+        #     if i==0:
+        #         z+=1
+        #         ans+=z
+        #     else:
+        #         z=0
+        # return ans
+        
+nums = [1,3,0,0,2,0,0,4]
+print(Solution().zeroFilledSubarray(nums))
