35e6e12d9a94bc9b76120e284415d2b970abf0ef
Chunbo Liu <chunboo.liu@gmail.com>
2023-02-07 22:38:22 +0800

    0207 added the 520 number of ways dp method

---
+++ DPCodes/520-numberOfWays.py
@@ -0,0 +1,23 @@
+from functools import lru_cache
+class Solution:
+    def numberOfWays(self, startPos: int, endPos: int, k: int) -> int:    
+        @lru_cache
+        def f(s,e,kk):
+            d=abs(s-e)
+            if d>kk:
+                return 0
+            # if d&1 !=kk&1:
+            #     return 0
+            if kk==0:
+                if s==e:
+                    return 1
+                else:
+                    return 0
+            return f(s-1,e,kk-1)+f(s+1,e,kk-1)
+        return f(startPos,endPos,k)%(10**9+7)
+    
+    
+start=1
+end=2
+k=3
+print(Solution().numberOfWays(start,end,k))
