80e38890971d1ad928df8cdc5d2a79a573d69076
Chunbo Liu <chunboo.liu@gmail.com>
2023-02-15 22:48:15 +0800

    0215-updated the gcd codes of 529

--- Math/529-commonFactors.py
+++ Math/529-commonFactors.py
@@ -4,9 +4,9 @@ class Solution:
             while b:
                 a,b=b,a%b
             return a
-            if b:
-                return gcd(b,a%b)
-            return a
+            # if b:
+            #     return gcd(b,a%b)
+            # return a
         ans=1
         x=gcd(a,b)
         for i in range (2,x+1):
