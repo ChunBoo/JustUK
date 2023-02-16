473e605ee36ce5babb0f3417153e9f81e5764a14
Chunbo Liu <chunboo.liu@gmail.com>
2023-02-16 22:24:01 +0800

    0216-added the 533 have conflicts in two times

---
+++ Math/533-haveConflict.py
@@ -0,0 +1,19 @@
+class Solution:
+    def haveConflict(self, event1: List[str], event2: List[str]) -> bool:
+        a1,a2=event1
+        b1,b2=event2
+        return max(a1,b1)<=min(a2,b2)
+        # endTime1H,endTime1M=event1[-1].split(":")
+        # sTime1H,sTime1M=event1[0].split(":")
+        # sTime2H,sTime2M=event2[0].split(":")
+        # endTime2H,endTime2M=event2[-1].split(":")
+        # if sTime2H<endTime1H:
+        #     return True
+        # elif sTime2H==endTime1H and sTime2M<=endTime1M:
+        #     return True
+        # if endTime2H>sTime1H:
+        #     return True
+        # elif endTime2H==sTime1H and endTime2M<=sTime1M:
+        #     return True
+        # # elif sTime2H==endTime1H an
+        # return False
