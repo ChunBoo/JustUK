00e06380d1cff307a47311aefc7df73e256bd227
Chunbo Liu <chunboo.liu@gmail.com>
2023-03-23 21:36:22 +0800

    0323 updated the 17 with correct codes

--- backtracking/17-100-letterCombinations.cpp
+++ backtracking/17-100-letterCombinations.cpp
@@ -3,34 +3,38 @@
 #include<vector>
 
 using std::cout;
-using STR=std::string;
-using VEC=std::vector<STR>;
+using string=std::string;
+using vector=std::vector<string>;
 
-void foo(STR cur,int begin,VEC& res,const STR& d,const VEC& mm)
-{
-    if(begin==d.size())
+class Solution {
+public:
+
+    void dfs(int begin,string cur,int totalLen,vector& mm,vector& ans,string& digits)
     {
-        res.push_back(cur);
-        return ;
+        if(begin==totalLen)
+        {
+            ans.push_back(cur);
+            return;
+        }
+        int digit=digits[begin]-'0';
+        string curLetters=mm[digit];
+        for(auto c:curLetters)
+            dfs(begin+1,cur+c,totalLen,mm,ans,digits);
     }
-    for(auto &c:mm[begin])
-    {
-        foo(cur+c,begin+1,res,d,mm);
+    vector letterCombinations(string digits) {
+        int n=digits.size();
+        vector ans{};
+        vector mm{"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+
+        if(n==0)
+            return ans;
+        dfs(0,"",n,mm,ans,digits);
+        return ans;
     }
-}
-
-VEC letCombination(const STR& digits)
-{
-    int n=digits.size();
-    VEC ans{};
-    VEC m{"abc","def"};
-    foo("",0,ans,digits,m);
-    return ans;
-}
-
+};
 int main()
 {
-    VEC ret=letCombination("23");
+    vector ret=Solution().letterCombinations("3");
     for(auto s:ret)
         cout<<s<<',';
 }
