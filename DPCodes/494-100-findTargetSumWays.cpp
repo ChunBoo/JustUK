b61003508602a814ca4317d654bec62852166267
cbliu <chunboo.liu@gmail.com>
2023-03-27 12:22:55 +0800

    0327 updated the cpp version of 494-100

--- DPCodes/494-100-findTargetSumWays.cpp
+++ DPCodes/494-100-findTargetSumWays.cpp
@@ -8,9 +8,9 @@ int foo(const VEC& nums,int idx,int t)
 {
     // if(t<0)
         // return 0;
-    if(t<0)
+    if(idx<0)
     {
-        if(idx==int(nums.size())) return 1;
+        if(t==0) return 1;
         return 0;
     }
     if(t<nums[idx])
@@ -20,14 +20,14 @@ int foo(const VEC& nums,int idx,int t)
 
 int test(const VEC& nums,int t)
 {
-    // int n=nums.size();
+    int n=nums.size();
     int sum=std::accumulate(nums.begin(),nums.end(),0);
     t+=sum;
     if(t<0 || t&1)
         return 0;
     t/=2;
     // int ans=0;
-    return foo(nums,0,t); 
+    return foo(nums,n-1,t); 
     // return ans;
 }
 int main()
