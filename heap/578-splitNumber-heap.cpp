2102f551aad37af9b98d7c22c941763dcf8b9e03
Chunbo Liu <chunboo.liu@gmail.com>
2023-03-31 22:40:49 +0800

    0331 update the cpp codes of 578

--- heap/578-splitNumber-heap.cpp
+++ heap/578-splitNumber-heap.cpp
@@ -12,22 +12,24 @@ int foo(int n)
         nums.push_back(n%10);
         n/=10;
     }
-    std::sort(nums.begin(),nums.end());
+    std::sort(nums.begin(),nums.end(),std::greater<int>());
     // std::make_heap(nums.begin(),nums.end());
     int s1=0,s2=0;
     // std::sort_heap(nums.begin(),nums.end());
-    int nn=nums.size();
-    // while(!nums.empty())
-    for(int i=0;i<nn-1;++i)
+    // int nn=nums.size();
+    while(!nums.empty())
+    // for(int i=0;i<nn-1;++i)
     {
-        int cur=nums.front();
+        int cur=nums.back();
         s1=s1*10+cur;
-        nums.erase(nums.begin()+i);
+        nums.pop_back();
+    //     nums.erase(nums.begin()+i);
         if(!nums.empty())
         {
-            cur=nums.front();
+            cur=nums.back();
             s2=s2*10+cur;
-            nums.erase(nums.begin()+i+1);
+            nums.pop_back();
+    //         nums.erase(nums.begin()+i+1);
         }
     }
     return s1+s2;
