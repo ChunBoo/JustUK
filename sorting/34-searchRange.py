2ba057637a1e83fb09e6ce1d125a858881cc6b77
Chunbo Liu <chunboo.liu@gmail.com>
2023-04-02 22:57:18 +0800

    0402-added the search range

---
+++ sorting/34-searchRange.py
@@ -0,0 +1,23 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        
+        n=len(nums)
+        s,e=-1,-1
+        if n==0:
+            return [s,e]
+        if target<nums[0] or target>nums[n-1]:
+            return [s,e]
+        if target==nums[0] and n==1:
+            return [0,0]
+        l,r=0,n-1
+        while l<=r:
+            mid=(l+r)//2
+            if target==nums[mid]:
+                s=mid
+                e=mid
+                while(s>0 and nums[s]==nums[s-1]): s-=1
+                while(e<n-1 and nums[e]==nums[e+1]): e+=1
+                return [s,e]
+            elif target>nums[mid]:l+=1
+            else: r-=1;
+        return [s,e]
