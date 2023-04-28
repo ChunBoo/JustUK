28f1b4b9255ccefe0b139fb461aca7d67baa07d7
cbliu <chunboo.liu@gmail.com>
2023-04-28 10:55:58 +0800

    0428 added the review codes of 617

--- Tree/100-617-mergeTrees.cpp
+++ Tree/100-617-mergeTrees.cpp
@@ -1,25 +1,42 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
  */
+
+#include<iostream>
+#include "../treeNode.h"
 class Solution {
 public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(!root1)
-            return root2;
         if(!root2)
             return root1;
-        
-        root1->val+=root2->val;
+        if(!root1)
+            return root2;
+        root1->m_val+=root2->m_val;
         root1->left=mergeTrees(root1->left,root2->left);
         root1->right=mergeTrees(root1->right,root2->right);
         return root1;
     }
 };
+
+int main()
+{
+    TreeNode *r1=new TreeNode(1);
+    TreeNode *left1=new TreeNode(2);
+    TreeNode *right1=new TreeNode(3);
+    
+    r1->left=left1;
+    r1->right=right1;
+
+    TreeNode *r2=new TreeNode(1);
+    TreeNode *left2=new TreeNode(2);
+    TreeNode *right2=new TreeNode(3);
+    
+    r2->left=left2;
+    r2->right=right2;
+
+    TreeNode *nr=Solution().mergeTrees(r1,r2);
+
+    TreeNode::printTreeInOrder(nr);
+    std::cout<<'\n';
+    TreeNode::printTreeNodePreOrder(nr);
+}
