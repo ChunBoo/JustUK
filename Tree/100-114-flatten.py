b552d2234b8cf3b6ed71a6a8ffa0832201f5ffb4
Chunbo Liu <chunboo.liu@gmail.com>
2023-05-04 23:10:18 +0800

    0504-added the 114

---
+++ Tree/100-114-flatten.py
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution:
+    def flatten(self, root: TreeNode) -> None:
+        curr = root
+        while curr:
+            if curr.left:
+                predecessor = nxt = curr.left
+                while predecessor.right:
+                    predecessor = predecessor.right
+                predecessor.right = curr.right
+                curr.left = None
+                curr.right = nxt
+            curr = curr.right
+
