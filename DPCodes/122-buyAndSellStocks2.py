675e310f8c0bcb0eec8018f58c50c0f2b211031f
cbliu <chunboo.liu@gmail.com>
2023-05-29 09:17:35 +0800

    0529 added the reviewing codes

--- DPCodes/122-buyAndSellStocks2.py
+++ DPCodes/122-buyAndSellStocks2.py
@@ -1,11 +1,13 @@
+from functools import cache
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
+    def maxProfit(self, prices):#List[int]) -> int:
         n=len(prices)
         @cache
         def dp(i):
             if i<0:
-                return 0,-10**9
-            
-            profit,balance=dp(i-1)
-            return max(profit,balance+prices[i]),max(balance,profit-prices[i])
+                return 0,-10**9 #maxProfit, balance
+            profit, balance=dp(i-1)
+            return max(profit, balance+prices[i]), max(balance, profit-prices[i])
         return dp(n-1)[0]
+prices = [7,1,5,3,6,4]
+print(Solution().maxProfit(prices))
--- multipleThread/1114-printInOrder.cpp
+++ multipleThread/1114-printInOrder.cpp
@@ -16,35 +16,61 @@ Input: nums = [1,2,3]
 Output: "firstsecondthird"
 Explanation: There are three threads being fired asynchronously. The input [1,2,3] means thread A calls first(), thread B calls second(), and thread C calls third(). "firstsecondthird" is the correct output.
 */
+#include<iostream>
+#include<mutex>
+#include<functional>
+#include<thread>
+
+using namespace std;
 
 class Foo {
-public:
+
     private:
-     mtx2,mtx3;
+     mutex mtx2,mtx3;
+     public:
     Foo() {
         mtx2.lock();
         mtx3.lock();
         // mtx1.unlock();
     }
-
-    void first(function<void()> printFirst) {
+    public:
+    // void first(function<void()> printFirst) {
+        void first(){
         // mtx1.lock();
         // printFirst() outputs "first". Do not change or remove this line.
-        printFirst();
+        // printFirst();
+        cout<<"1\n";
         mtx2.unlock();
     }
 
-    void second(function<void()> printSecond) {
+    // void second(function<void()> printSecond) {
+        void second(){
         mtx2.lock();
         // printSecond() outputs "second". Do not change or remove this line.
-        printSecond();
+        // printSecond();
+        cout<<"2\n";
         mtx3.unlock();
     }
 
-    void third(function<void()> printThird) {
+    // void third(function<void()> printThird) {
+        void third(){
         mtx3.lock();
         // printThird() outputs "third". Do not change or remove this line.
-        printThird();
+        // printThird();
+        cout<<"3\n";
         // mtx1.unlock();
     }
 };
+
+int main()
+{
+    Foo foo;
+    thread t1(&Foo::first,foo);
+    thread t2(&Foo::second,foo);
+    thread t3(&Foo::third,foo);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+}
