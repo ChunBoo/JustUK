71e8bd8fefc30d70638fe66884fc7a8ef0c78a6c
cbliu <chunboo.liu@gmail.com>
2023-06-08 08:32:21 +0800

    0608 added the reviewing codes of 763

--- greedy/763-partitionLabels.cpp
+++ greedy/763-partitionLabels.cpp
@@ -1,21 +1,41 @@
+#include<iostream>
+#include<vector>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+using VEC=std::vector<int>;
+
 class Solution {
 public:
-    vector<int> partitionLabels(string s) {
-        vector<int> lastIndex(128,0);
-        for(int i=0;i<s.size();++i)
+    VEC partitionLabels(const string& s) {
+        int n=s.size();
+        VEC ans{};
+        VEC lastIndex(128,0);
+        for(int i=0;i<n;++i)
+        {
             lastIndex[s[i]]=i;
-        
-        vector<int> ans{};
+        }
 
         int start=0,end=0;
-        for(int i=0;i<s.size();++i)
+        for(int i=0;i<n;++i)
         {
             end=max(end,lastIndex[s[i]]);
-            if(end==i){
+            if(end==i)
+            {
                 ans.push_back(end-start+1);
-                start=end+1;
+                start=end+1;  //here should update the start as end+1
             }
         }
         return ans;
     }
 };
+
+int main()
+{
+    string s{"ababcbacadefegdehijhklij"};
+    vector<int> res=Solution().partitionLabels(s);
+    for(auto v:res)
+        cout<<v<<',';
+}
