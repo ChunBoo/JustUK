13214ed3493a51f894bb7a7f35d1831ba84cd95f
cbliu <chunboo.liu@gmail.com>
2023-06-14 08:51:36 +0800

    0614 added the reviewing codes of 668

--- sorting/668-findKthNumber.cpp
+++ sorting/668-findKthNumber.cpp
@@ -1,11 +1,12 @@
 /**
  * 
- * Nearly everyone has used the Multiplication Table. The multiplication table of size m x n is an integer matrix mat where mat[i][j] == i * j (1-indexed).
-
-Given three integers m, n, and k, return the kth smallest element in the m x n multiplication table.
-
-
+ * Nearly everyone has used the Multiplication Table. 
+   The multiplication table of size m x n is an integer matrix mat where mat[i][j] == i * j (1-indexed).
+   Given three integers m, n, and k, return the kth smallest element in the m x n multiplication table.
 */
+#include<iostream>
+#include<cmath>
+using std::cout;
 
 class Solution {
 public:
@@ -24,7 +25,14 @@ private:
     int LEX(int m,int n,int x){
         int count=0;
         for(int i=1;i<=m;++i)
-            count+=min(n,x/i);
+            count+=std::min(n,x/i);
         return count;
     }
 };
+
+int main()
+{
+    int m=3,n=3,k=7;
+    int res=Solution().findKthNumber(m,n,k);
+    cout<<res<<'\n';
+}
