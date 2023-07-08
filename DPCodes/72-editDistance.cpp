174fd0c3cdd0870cbe6267de3e8a4c23a75e3734
Chunbo Liu <chunboo.liu@gmail.com>
2023-07-08 23:14:58 +0800

    0708-added the 923 combination of math issue

--- DPCodes/72-editDistance.cpp
+++ DPCodes/72-editDistance.cpp
@@ -23,3 +23,22 @@ private:
         return d_[l1][l2]=ans;
     }
 };
+class Solution2 {
+public:
+    int minDistance(string word1, string word2) {
+        int l1=word1.length(),l2=word2.length();
+        vector<vector<int>> dp(l1+1,vector<int>(l2+1,0));
+
+        for(int i=0;i<=l1;++i)
+            dp[i][0]=i;
+        for(int j=0;j<=l2;++j)
+            dp[0][j]=j;
+        
+        for(int i=1;i<=l1;++i)
+            for(int j=1;j<=l2;++j){
+                int c=(word1[i-1]==word2[j-1])?0:1;
+                dp[i][j]=min(dp[i-1][j-1]+c,min(dp[i-1][j],dp[i][j-1])+1);
+            }
+        return dp[l1][l2];
+    }
+};
