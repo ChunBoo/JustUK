936e447ba2991260698f77c38274bf8e45a9a23a
Chunbo Liu <chunboo.liu@gmail.com>
2023-07-12 23:01:11 +0800

    0712-added the 847

---
+++ DFS_BFS/847-bfsShortestPathLength.cpp
@@ -0,0 +1,39 @@
+/**
+ * You have an undirected, connected graph of n nodes labeled from 0 to n - 1. You are given an array graph where graph[i] is a list of all the nodes connected with node i by an edge.
+
+Return the length of the shortest path that visits every node. You may start and stop at any node, you may revisit nodes multiple times, and you may reuse edges.
+Input: graph = [[1,2,3],[0],[0],[0]]
+Output: 4
+Explanation: One possible path is [1,0,2,0,3]
+
+*/
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& graph) {
+        const int n=graph.size();
+        const int kAns=(1<<n)-1;
+        queue<pair<int,int>> q;
+        vector<vector<int>> visited(n,vector<int>(1<<n));
+        for(int i=0;i<n;++i)
+            q.push({i,1<<i});
+        int steps=0;
+        while(!q.empty())
+        {
+            int s=q.size();
+            while(s--)
+            {
+                auto p=q.front();
+                q.pop();
+                int node=p.first;
+                int sts=p.second;
+                if(sts==kAns) return steps;
+                if(visited[node][sts]) continue;
+                visited[node][sts]=1;
+                for(int nxt:graph[node])
+                    q.push({nxt,sts|(1<<nxt)});
+            }
+            ++steps;
+        }
+        return -1;
+    }
+};
