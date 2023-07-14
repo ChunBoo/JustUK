fd0cb532983374d9a9b1a2d41522057b0c56b736
cbliu <chunboo.liu@gmail.com>
2023-07-14 08:58:42 +0800

    0714 added the review codes of 307

--- datastructure/307-range-sum-query-mutable.cpp
+++ datastructure/307-range-sum-query-mutable.cpp
@@ -1,18 +1,17 @@
 
 /**
  * Given an integer array nums, handle multiple queries of the following types:
-
 Update the value of an element in nums.
-Calculate the sum of the elements of nums between indices left and right inclusive where left <= right.
-Implement the NumArray class:
+Calculate the sum of the elements of nums between indices left and right
+inclusive where left <= right. Implement the NumArray class:
 
 NumArray(int[] nums) Initializes the object with the integer array nums.
 void update(int index, int val) Updates the value of nums[index] to be val.
-int sumRange(int left, int right) Returns the sum of the elements of nums between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ... + nums[right]).
- 
+int sumRange(int left, int right) Returns the sum of the elements of nums
+between indices left and right inclusive (i.e. nums[left] + nums[left + 1] + ...
++ nums[right]).
 
 Example 1:
-
 Input
 ["NumArray", "sumRange", "update", "sumRange"]
 [[[1, 3, 5]], [0, 2], [1, 2], [0, 2]]
@@ -24,25 +23,31 @@ NumArray numArray = new NumArray([1, 3, 5]);
 numArray.sumRange(0, 2); // return 1 + 3 + 5 = 9
 numArray.update(1, 2);   // nums = [1, 2, 5]
 numArray.sumRange(0, 2); // return 1 + 2 + 5 = 8
-
- * 
+ *
 */
+#include <iostream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+
 class SegTreeNode{
     public:
-        SegTreeNode(int start,int end,int sum,SegTreeNode* left=nullptr, SegTreeNode* right=nullptr):
-            start(start),
-            end(end),
-            sum(sum),
-            left(left),
-            right(right){}
-        SegTreeNode(const SegTreeNode&)=delete;
-        SegTreeNode& operator=(const SegTreeNode&)=delete;
-        ~SegTreeNode()
-        {
-            delete right;
-            delete left;
-            left=right=nullptr;
-        }
+      SegTreeNode(int start, int end, int sum, SegTreeNode *left = nullptr,
+                  SegTreeNode *right = nullptr)
+          : start(start), end(end), sum(sum), left(left), right(right) {
+        cout << "ctor segTreeNode now.\n";
+      }
+      SegTreeNode(const SegTreeNode &) = delete; // remove the copy
+                                                 // constructor
+      SegTreeNode &
+      operator=(const SegTreeNode &) = delete; // remove the operator=
+      ~SegTreeNode() {
+        cout << "dtor segTreeNode now.\n";
+        delete right;
+        delete left;
+        left = right = nullptr;
+      }
         int start;
         int end;
         int sum;
@@ -52,57 +57,54 @@ class SegTreeNode{
 
 class NumArray {
 private:
-    vector<int> nums_;
-    std::unique_ptr<SegTreeNode> root_;
-    SegTreeNode* buildTree(int start,int end){
-        if(start==end)
-            return new SegTreeNode(start,end,nums_[start]);
-        int mid=start+(end-start)/2;
-        auto left=buildTree(start,mid);;
-        auto right=buildTree(mid+1,end);
-        auto node=new SegTreeNode(start,end,left->sum+right->sum,left,right);
-        return node;
+  std::unique_ptr<SegTreeNode> m_root;
+  vector<int> m_nums;
+
+  SegTreeNode *buildTree(int start, int end) {
+    if ((start == end)) {
+      return new SegTreeNode(start, end, m_nums[start]);
     }
-    void updateTree(SegTreeNode* root,int i,int val){
-        if(root->start==i &&root->end==i)
-        {
-            root->sum=val;
-            return;
-        }
-        int mid=root->start+(root->end-root->start)/2;
-        if(i<=mid)
-            updateTree(root->left,i,val);
-        else
-            updateTree(root->right,i,val);
-        root->sum=root->left->sum+root->right->sum;
+    int mid = start + (end - start) / 2;
+    auto left = buildTree(start, mid);
+    auto right = buildTree(mid + 1, end);
+    return new SegTreeNode(start, end, left->sum + right->sum, left, right);
+  }
+  void updateTree(SegTreeNode *root, int idx, int val) {
+    if (root->start == idx && root->end == idx) {
+      root->sum = val;
+      return;
     }
-    int sumRange(SegTreeNode* root,int i,int j){
-        if(i==root->start&&j==root->end){
-            return root->sum;
-        }
-        int mid=root->start+(root->end-root->start)/2;
-        if(j<=mid){
-            return sumRange(root->left,i,j);
-        }
-        else if(i>mid)
-            return sumRange(root->right,i,j);
-        else
-            return sumRange(root->left,i,mid)+sumRange(root->right,mid+1,j);
-    }
+    int mid = root->start + (root->end - root->start) / 2;
+    if (idx <= mid)
+      updateTree(root->left, idx, val);
+    else
+      updateTree(root->right, idx, val);
+    root->sum = root->left->sum + root->right->sum;
+  }
+
+  int sumRange(SegTreeNode *root, int i, int j) {
+    if (root->start == i && root->end == j)
+      return root->sum;
+    int mid = root->start + (root->end - root->start) / 2;
+    if (i <= mid)
+      return sumRange(root->left, i, j);
+    else if (j > mid)
+      return sumRange(root->right, i, j);
+    else
+      return sumRange(root->left, i, mid) + sumRange(root->right, mid + 1, j);
+  }
+
 public:
-    NumArray(vector<int>& nums) {
-        nums_.swap(nums);
-        if(!nums_.empty())
-            root_.reset(buildTree(0,nums_.size()-1));
-    }
-    
-    void update(int index, int val) {
-        updateTree(root_.get(),index,val);
-    }
-    
-    int sumRange(int left, int right) {
-        return sumRange(root_.get(),left,right);
-    }
+  NumArray(vector<int> &&nums) {
+    m_nums.swap(nums);
+    m_root.reset(buildTree( 0, m_nums.size() - 1));
+  }
+
+  void update(int index, int val) { updateTree(m_root.get(), index, val); }
+
+  int sumRange(int left, int right) {
+    return sumRange(m_root.get(), left, right);
+  }
 };
 
 /**
@@ -111,3 +113,10 @@ public:
  * obj->update(index,val);
  * int param_2 = obj->sumRange(left,right);
  */
+
+int main() {
+  NumArray numArray{vector<int>{1, 3, 5}};
+  cout << numArray.sumRange(0, 2) << '\n';
+  numArray.update(1, 4);
+  cout << numArray.sumRange(0, 2) << '\n';
+}
