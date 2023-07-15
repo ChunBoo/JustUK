8701a1a77c342577c8c5ca3aaec1a63a70380eb0
Chunbo Liu <chunboo.liu@gmail.com>
2023-07-15 23:27:23 +0800

    0715 added　460　O-1ｍｅｔｈｏｄ

--- datastructure/460-LFU.cpp
+++ datastructure/460-LFU.cpp
@@ -108,3 +108,80 @@ public:
  * int param_1 = obj->get(key);
  * obj->put(key,value);
  */
+
+
+struct CacheNode{
+    int key;
+    int value;
+    int freq;
+    list<int>::const_iterator it;  //pointer to the node in the list
+};
+
+
+class LFUCache {
+private:
+    int capacity_;
+    int min_freq_;
+    //key--->CacheNode
+    unordered_map<int,CacheNode> n_;
+    //freq-->keys with freq
+    unordered_map<int,list<int>> l_;
+    void touch(CacheNode& node){
+        //s1:update the freq
+        const int prev_freq=node.freq;
+        const int freq=++(node.freq);
+
+        //s2:remove the entry from old freq_list
+        l_[prev_freq].erase(node.it);
+        //s3
+        if(l_[prev_freq].empty()&&prev_freq==min_freq_){
+            //del the lsit
+            l_.erase(prev_freq);
+            ++min_freq_;
+        }
+        //s4, insert the key into the front of the new freq list
+        l_[freq].push_front(node.key);
+        //s5: update the pointer
+        node.it=l_[freq].cbegin();
+    }
+public:
+    LFUCache(int capacity):capacity_(capacity),min_freq_(0){}
+    
+    int get(int key) {
+        auto it=n_.find(key);
+        if(it==n_.cend()) return -1;
+        touch(it->second);
+        return it->second.value;
+    }
+    
+    void put(int key, int value) {
+        if(capacity_==0) return;
+        auto it=n_.find(key);
+        if(it!=n_.cend()){
+            it->second.value=value;
+            touch(it->second);
+            return ;
+        }
+        if(n_.size()==capacity_){
+            //no capacity, remove one entry that has lowest freq or least recently used if there are mul one
+            //s1:remove the element from m_freq_list
+            const int key_to_evict=l_[min_freq_].back();
+            l_[min_freq_].pop_back();
+            //s2:remove the key from cache
+            n_.erase(key_to_evict);
+        }
+        const int freq=1;
+        min_freq_=freq;
+        //add the key to the freq list
+        l_[freq].push_front(key);
+        //create new node
+        n_[key]={key,value,freq,l_[freq].cbegin()};
+    }
+};
+
+/**
+ * Your LFUCache object will be instantiated and called as such:
+ * LFUCache* obj = new LFUCache(capacity);
+ * int param_1 = obj->get(key);
+ * obj->put(key,value);
+ */
