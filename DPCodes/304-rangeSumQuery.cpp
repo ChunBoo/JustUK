4d4ed0f6b84ed9ec42d333af0d5c82cf00c9c43e
Chunbo Liu <chunboo.liu@gmail.com>
2023-07-20 22:52:34 +0800

    0720 added the 304

---
+++ DPCodes/304-rangeSumQuery.cpp
@@ -0,0 +1,48 @@
+/**
+ * 
+ * Given a 2D matrix matrix, handle multiple queries of the following type:
+
+Calculate the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+Implement the NumMatrix class:
+
+NumMatrix(int[][] matrix) Initializes the object with the integer matrix matrix.
+int sumRegion(int row1, int col1, int row2, int col2) Returns the sum of the elements of matrix inside the rectangle defined by its upper left corner (row1, col1) and lower right corner (row2, col2).
+You must design an algorithm where sumRegion works on O(1) time complexity.
+
+*/
+
+class NumMatrix {
+private:
+    vector<vector<int>> sums_;
+public:
+    NumMatrix(vector<vector<int>>& matrix) {
+        sums_.clear();
+        if(matrix.empty()) return;
+        int m=matrix.size();
+        int n=matrix[0].size();
+
+        //sum[i][j]=sum(matrix[0][0]-matrix[i-1][j-1])
+        sums_=vector<vector<int>>(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;++i)
+        {
+            for(int j=1;j<=n;++j)
+                sums_[i][j]=matrix[i-1][j-1]
+                            +sums_[i-1][j]
+                            +sums_[i][j-1]
+                            -sums_[i-1][j-1];
+        }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        return sums_[row2+1][col2+1]
+                -sums_[row2+1][col1]
+                -sums_[row1][col2+1]
+                +sums_[row1][col1];
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
