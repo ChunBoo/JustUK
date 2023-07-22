9becdcd19453a6ad2c5f7e050539819c6f3b5aca
Chunbo Liu <chunboo.liu@gmail.com>
2023-07-22 23:14:57 +0800

    0722-added the 221 maximum square

---
+++ DPCodes/221-maximumSquare.cpp
@@ -0,0 +1,34 @@
+class Solution {
+private:
+    vector<vector<int>> sums_;
+public:
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m=matrix.size();
+        int n=matrix[0].size();
+        sums_=vector<vector<int>>(m+1,vector<int>(n+1,0));
+
+        for(int i=1;i<=m;++i)
+        {
+            for(int j=1;j<=n;++j)
+            {
+                sums_[i][j]=matrix[i-1][j-1]-'0'+sums_[i][j-1]+sums_[i-1][j]-
+                                sums_[i-1] [j-1];
+            }
+        }
+        int ans=0;
+        for(int i=1;i<=m;++i)
+        {
+            for(int j=1;j<=n;++j)
+            {
+                for(int k=min(m-i+1,n-j+1);k>0;--k)
+                {
+                    int tmp=sums_[i+k-1][j+k-1]-sums_[i+k-1][j-1]-sums_[i-1][j+k-1]+sums_[i-1][j-1];
+                    if(tmp==k*k){
+                    ans=max(tmp,ans);
+                    break;}
+                }
+            }
+        }
+        return ans;
+    }
+};
