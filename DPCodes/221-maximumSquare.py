fb3688a024046bd3333cf45cea0db8b0b185ae7d
cbliu <chunboo.liu@gmail.com>
2023-07-25 08:45:51 +0800

    0725 added the reviewing codes of 221 and 530

--- DPCodes/221-maximumSquare.py
+++ DPCodes/221-maximumSquare.py
@@ -1,10 +1,13 @@
 
 
+
+
 def maxSquare(m):
     r=len(m)
     c=len(m[0])
 
-    s=[[0 for i in range(c+1)] for j in range(m+1)]
+    s = [[0 for _ in range(c+1)] for _ in range(r+1)]
+    # s = [[0 for i in range(c+1)] for j in range(r+1)]
 
     for i in range(1,r+1):
         for j in range(1,c+1):
@@ -25,5 +28,5 @@ def maxSquare(m):
 #    [1,1,1,1,1],
 #    [1,0,0,1,0]]
 m=[[1,1,1],
-   [1,0,1]]
+   [1,1,1]]
 print(maxSquare(m))           
--- Tree/530-getMinDifferenceBST.cpp
+++ Tree/530-getMinDifferenceBST.cpp
@@ -1,15 +1,23 @@
 /**
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
  */
-class Solution {
+ struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+using TN=struct TreeNode;
+
+class Solution2 {
 private:
     void inorder(TreeNode* root,vector<int>& nodes)
     {
@@ -23,7 +31,7 @@ public:
         vector<int> nodes;
         inorder(root,nodes);
         int minDiff=nodes.back();
-        for(int i=1;i<nodes.size();++i)
+        for(size_t i=1;i<nodes.size();++i)
             minDiff=min(minDiff,nodes[i]-nodes[i-1]);
         return minDiff;
         
@@ -46,23 +54,31 @@ class Solution {
 private:
     int* prev_;
     int minDiff;
-    void inorder(TreeNode* root)
-    {
-        if(!root)   return;
+    void inorder(TreeNode* root){
+        if(!root)
+            return;
         inorder(root->left);
         if(prev_)
-            minDiff=min(minDiff,root->val-*(prev_));
-        prev_=&root->val;
+            minDiff=min(minDiff,root->val-*prev_);
+        prev_=&(root->val);
         inorder(root->right);
     }
 public:
     int getMinimumDifference(TreeNode* root) {
-        vector<int> nodes;
         minDiff=INT_MAX;
         prev_=nullptr;
         inorder(root);
-
         return minDiff;
-        
     }
 };
+
+int main() {
+  TN *root = new TN(10);
+  TN *one = new TN(9);
+  TN *two = new TN(12);
+
+  root->left = one;
+  // two->right=one;
+  root->right = two;
+  std::cout << Solution().getMinimumDifference(root);
+}
