9f9e3dbae37d0d49c0589701e32e069b9935de05
Chunbo Liu <chunboo.liu@gmail.com>
2023-08-13 23:20:14 +0800

    0813 added 329 DFS

---
+++ DFS_BFS/329-longestIncreasingPath-dfs.cpp
@@ -0,0 +1,74 @@
+/**
+ * Given an m x n integers matrix, return the length of the longest increasing path in matrix.
+
+From each cell, you can either move in four directions: left, right, up, or down. You may not move diagonally or move outside the boundary (i.e., wrap-around is not allowed).
+Input: matrix = [[9,9,4],[6,6,8],[2,1,1]]
+Output: 4
+Explanation: The longest increasing path is [1, 2, 6, 9].
+*/
+
+class Solution {
+private:
+    int m_;
+    int n_;
+    vector<vector<int>> dp_;
+    int dfs(const vector<vector<int>>& mat,int x,int y){
+        if(dp_[y][x]!=-1) return dp_[y][x];
+        static int dirs[]={0,-1,0,1,0};
+        dp_[y][x]=1;
+        for(int i=0;i<4;++i){
+            int tx=x+dirs[i];  //col
+            int ty=y+dirs[i+1]; //row
+            if(tx<0||ty<0||tx>=n_||ty>=m_||mat[ty][tx]<=mat[y][x])
+                continue;
+            dp_[y][x]=max(dp_[y][x],1+dfs(mat,tx,ty));
+        }
+        return dp_[y][x];
+    }
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if(matrix.empty()) return 0;
+        m_=matrix.size();
+        n_=matrix[0].size();
+        dp_=vector<vector<int>>(m_,vector<int>(n_,-1));
+        int ans=0;
+        for(int y=0;y<m_;++y)  //start from each element
+            for(int x=0;x<n_;++x)
+                ans=max(ans,dfs(matrix,x,y));
+        return ans;
+    }
+};
+
+class Solution {  //bottom-up
+
+public:
+    int longestIncreasingPath(vector<vector<int>>& matrix) {
+        if(matrix.empty()) return 0;
+        int m=matrix.size(),n=matrix[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,1));
+
+        int ans=0;
+        vector<pair<int,pair<int,int>>> cells;
+        for(int y=0;y<m;++y)
+            for(int x=0;x<n;++x)
+                cells.push_back({matrix[y][x],{x,y}});
+        sort(cells.rbegin(),cells.rend());
+
+        vector<int> dirs{0,1,0,-1,0};
+
+        for(const auto& cell:cells)
+        {
+            int x=cell.second.first;
+            int y=cell.second.second;//row
+            for(int i=0;i<4;++i){
+                int tx=x+dirs[i];
+                int ty=y+dirs[i+1];
+                if(tx<0||tx>=n||ty<0||ty>=m) continue;
+                if(matrix[ty][tx]<=matrix[y][x]) continue;
+                dp[y][x]=max(dp[y][x],1+dp[ty][tx]);
+            }
+            ans=max(ans,dp[y][x]);
+        }
+        return ans;
+    }
+};
