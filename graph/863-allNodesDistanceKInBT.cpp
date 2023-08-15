2c32691d64f9e074b37aeaaf2a73af2260317006
cbliu <chunboo.liu@gmail.com>
2023-08-15 08:11:51 +0800

    0815 added reviewing codes of 784 and 863

--- graph/863-allNodesDistanceKInBT.cpp
+++ graph/863-allNodesDistanceKInBT.cpp
@@ -1,25 +1,39 @@
 /**
- * Given the root of a binary tree, the value of a target node target, and an integer k, return an array of the values of all nodes that have a distance k from the target node.
+ * Given the root of a binary tree, the value of a target node target, and an
+integer k, return an array of the values of all nodes that have a distance k
+from the target node.
 
 You can return the answer in any order.
  * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
+
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+  TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
 class Solution {
 private:
     unordered_map<TreeNode*,vector<TreeNode*>> g;
     void buildGraph(TreeNode* parent,TreeNode* child)
     {
-        if(parent){
-            g[parent].push_back(child);
-            g[child].push_back(parent);
-        }
-        if(child->left) buildGraph(child,child->left);
+      // create the graph from the binary tree
+      if (parent) {
+        g[parent].push_back(child);
+        g[child].push_back(parent);
+      }
+        if (child->left)
+          buildGraph(child, child->left);
         if(child->right) buildGraph(child,child->right);
     }
 public:
@@ -27,25 +41,47 @@ public:
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         buildGraph(nullptr,root);
         vector<int> ans;
-        unordered_set<TreeNode*> seen;
-        queue<TreeNode*> q;
-        seen.insert(target);
+        queue<TreeNode *> q;
         q.push(target);
-
-        int k_=0;
-        while(!q.empty()&&k_<=k){
-            int s=q.size();
-            while(s--){
-                TreeNode* node=q.front();q.pop();
-                if(k==k_) ans.push_back(node->val);
-                for(TreeNode* child:g[node]){
-                    if(seen.count(child)) continue;
-                    q.push(child);
-                    seen.insert(child);
-                }
+        unordered_set<TreeNode *> seen{target};
+        int _k = 0;
+        while (!q.empty() && _k <= k) {
+          int size = q.size();
+          while (size--) {
+            TreeNode *cur = q.front();
+            q.pop();
+            if (_k == k)
+              ans.push_back(cur->val);
+            for (auto &nxt : g[cur]) {
+              if (seen.count(nxt))
+                continue;
+              seen.insert(nxt);
+              q.push(nxt);
             }
-            ++k_;
+          }
+          ++_k;
         }
         return ans;
     }
 };
+
+int main() {
+  TreeNode *root = new TreeNode(3);
+  TreeNode *one = new TreeNode(1);
+  TreeNode *two = new TreeNode(2);
+  TreeNode *four = new TreeNode(4);
+  TreeNode *five = new TreeNode(5);
+  TreeNode *six = new TreeNode(6);
+  TreeNode *seven = new TreeNode(7);
+
+  root->left = five;
+  root->right = one;
+  five->left = six;
+  five->right = seven;
+  seven->left = two;
+  seven->right = four;
+  int k = 1;
+  vector<int> res = Solution().distanceK(root, five, k);
+  for (int v : res)
+    cout << v << ',';
+}
