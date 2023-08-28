dc7099cd52c770907c969090de9457ce4a456eda
Chunbo Liu <chunboo.liu@gmail.com>
2023-08-28 22:53:28 +0800

    0828-added the dp example of 877

---
+++ DPCodes/877-stoneGame.cpp
@@ -0,0 +1,17 @@
+class Solution {
+private:
+    vector<vector<int>> m_;
+    int score(const vector<int>& piles,int l,int r){
+        if(l==r) return piles[l];
+        if(m_[l][r]==INT_MIN)
+            m_[l][r]=max(piles[l]-score(piles,l+1,r),
+                         piles[r]-score(piles,l,r-1));
+        return m_[l][r];
+    }
+public:
+    bool stoneGame(vector<int>& piles) {
+        const int n=piles.size();
+        m_=vector<vector<int>>(n,vector<int>(n,INT_MIN));
+        return score(piles,0,n-1);
+    }
+};
