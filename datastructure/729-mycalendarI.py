201a990ff29fa7f0829ac432817eeb643bedca72
cbliu <chunboo.liu@gmail.com>
2023-09-06 09:11:30 +0800

    0906 added the new method of python with bs

--- datastructure/729-mycalendarI.py
+++ datastructure/729-mycalendarI.py
@@ -1,5 +1,6 @@
 
-
+from bisect import bisect_left,bisect_right
+from collections import OrderedDict
 class CCalendar:
     def __init__(self):
         self.booked=[]
@@ -11,11 +12,24 @@ class CCalendar:
         self.booked.append((start,end))
         return True
 
+class CCalendar2:
+    def __init__(self):
+        self.map=OrderedDict()
+
+    def book(self,start,end):  #https://stackoverflow.com/questions/62731393/using-bisect-left-on-dict-raising-dict-is-not-a-sequence-error-how-to-t
+        for k,v in self.map.items():
+            if k>=start and v<=end:
+                return False
+            if 
+
+        return True
+
+
 v1=[10, 20]
 v2=[15, 25]
 v3=[20, 30]
 
-cal=CCalendar()
+cal=CCalendar2()
 print(cal.book(v1[0],v1[1]))
 print(cal.book(v2[0],v2[1]))
 print(cal.book(v3[0],v3[1]))
