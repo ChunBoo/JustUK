0f60edf9a28dd243444153440fde4675311e9a46
Chunbo Liu <chunboo.liu@gmail.com>
2023-09-07 23:28:01 +0800

    0907-added the 2416 prefix tree example

---
+++ datastructure/2416-sumOfPrefixString.cpp
@@ -0,0 +1,73 @@
+/**
+ * You are given an array words of size n consisting of non-empty strings.
+
+We define the score of a string word as the number of strings words[i] such that word is a prefix of words[i].
+
+For example, if words = ["a", "ab", "abc", "cab"], then the score of "ab" is 2, since "ab" is a prefix of both "ab" and "abc".
+Return an array answer of size n where answer[i] is the sum of scores of every non-empty prefix of words[i].
+
+Note that a string is considered as a prefix of itself.
+
+ 
+
+Example 1:
+
+Input: words = ["abc","ab","bc","b"]
+Output: [5,4,3,2]
+Explanation: The answer for each string is the following:
+- "abc" has 3 prefixes: "a", "ab", and "abc".
+- There are 2 strings with the prefix "a", 2 strings with the prefix "ab", and 1 string with the prefix "abc".
+The total is answer[0] = 2 + 2 + 1 = 5.
+- "ab" has 2 prefixes: "a" and "ab".
+- There are 2 strings with the prefix "a", and 2 strings with the prefix "ab".
+The total is answer[1] = 2 + 2 = 4.
+- "bc" has 2 prefixes: "b" and "bc".
+- There are 2 strings with the prefix "b", and 1 string with the prefix "bc".
+The total is answer[2] = 2 + 1 = 3.
+- "b" has 1 prefix: "b".
+- There are 2 strings with the prefix "b".
+The total is answer[3] = 2.
+Example 2:
+
+Input: words = ["abcd"]
+Output: [4]
+Explanation:
+"abcd" has 4 prefixes: "a", "ab", "abc", and "abcd".
+Each prefix has a score of one, so the total is answer[0] = 1 + 1 + 1 + 1 = 4.
+*/
+
+struct Trie{
+    Trie* ch[26]={};
+    int cnt=0;
+    void insert(string_view s){
+        Trie* cur=this;
+        for(char c:s){
+            if(!cur->ch[c-'a'])
+                cur->ch[c-'a']=new Trie();
+            cur=cur->ch[c-'a'];
+            ++cur->cnt;
+        }
+    }
+    int query(string_view s){
+        Trie* cur=this;
+        int ans=0;
+        for(char c:s){
+            cur=cur->ch[c-'a'];
+            ans+=cur->cnt;
+        }
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<int> sumPrefixScores(vector<string>& words) {
+        Trie* root=new Trie();
+        for(const string& w:words)
+            root->insert(w);
+        vector<int> ans;
+        for(const string& w:words)
+            ans.push_back(root->query(w));
+        return ans;
+    }
+};
