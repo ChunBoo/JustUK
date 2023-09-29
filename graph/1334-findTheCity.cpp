f8e1b1aa31743e1e48894c539494e5ace10b4bed
Chunbo Liu <chunboo.liu@gmail.com>
2023-09-29 11:28:27 +0800

    0929 added the reviewing codes of 1334 dijkstra

--- graph/1334-findTheCity.cpp
+++ graph/1334-findTheCity.cpp
@@ -30,3 +30,50 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<pair<int,int>>> g(n);
+        for(const auto&e:edges){
+            g[e[0]].emplace_back(e[1],e[2]);
+            g[e[1]].emplace_back(e[0],e[2]);
+        }
+        //return the  number of nodes within t from s.
+        auto dijkstra=[&](int s){
+            vector<int> dist(n,INT_MAX/2);
+            set<pair<int,int>> q;  //<dist,node>
+            vector<set<pair<int,int>>::const_iterator> its(n);
+            dist[s]=0;
+            for(int i=0;i<n;++i)
+                its[i]=q.emplace(dist[i],i).first;
+            while(!q.empty()){
+                auto it=cbegin(q);
+                int d=it->first;
+                int u=it->second;
+                q.erase(it);
+
+                if(d>distanceThreshold) break;
+                for(const auto&p:g[u]){
+                    int v=p.first;
+                    int w=p.second;
+                    if(dist[v]<=d+w) continue;
+                    //decrease key
+                    q.erase(its[v]);
+                    its[v] = q.emplace(dist[v] = d + w, v).first;
+                }
+            }
+            return count_if(begin(dist),end(dist),[distanceThreshold](int d){return d<=distanceThreshold;});
+            };
+            int ans=-1;
+            int min_nb=INT_MAX;
+            for(int i=0;i<n;++i){
+                int nb=dijkstra(i);
+                if(nb<=min_nb){
+                    min_nb=nb;
+                    ans=i;
+                }
+            }
+            return ans;
+    }
+};
