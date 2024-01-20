ee06c8673894602c825d2012d46a96b8c8fe75f2
Chunbo Liu <chunboo.liu@gmail.com>
2024-01-20 22:28:43 +0800

    0120-review the codes of dfs example of 79

--- backtracking/79-wordSearch.cpp
+++ backtracking/79-wordSearch.cpp
@@ -1,3 +1,23 @@
+
+/*
+Given a 2D board and a word, find if the word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cell, where “adjacent” cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+For example,
+Given board =
+[
+  ['A','B','C','E'],
+  ['S','F','C','S'],
+  ['A','D','E','E']
+]
+word = "ABCCED", -> returns true,
+word = "SEE", -> returns true,
+word = "ABCB", -> returns false.
+
+Idea:
+Search, depth first search
+*/
 class Solution {
 public:
     bool exist(vector<vector<char>>& board, string word) {
@@ -32,6 +52,41 @@ private:
                     ||search(board,r,c+1,d+1)
                     ||search(board,r,c-1,d+1);
         board[r][c]=cur;
+        return found;
+    }
+};
+
+
+class Solution {
+public:
+    bool exist(vector<vector<char>>& board, string word) {
+        r_=board.size();
+        c_=board[0].size();
+        word_=std::move(word);
+        for(int r=0;r<r_;++r)
+            for(int c=0;c<c_;++c)
+              if(dfs(board,r,c,0))
+                return true;
+        return false;
+    }
+
+private:
+    int r_;
+    int c_;
+    string word_;
+    bool dfs( vector<vector<char>>& board, int r,int c,int d)
+    {
+        if(r<0||r==r_||c<0||c==c_)
+            return false;
+        if(word_[d]!=board[r][c])
+            return false;
+        if(d==word_.size()-1)
+            return true;
+        char ch=board[r][c];
+        board[r][c]=0; //indicate current r/c already access, as each cell can be used only once
+        bool found= dfs(board,r+1,c,d+1)||dfs(board,r-1,c,d+1)
+              ||dfs(board,r,c+1,d+1)||dfs(board,r,c-1,d+1);
+        board[r][c]=ch;
         return found;
     }
 };
