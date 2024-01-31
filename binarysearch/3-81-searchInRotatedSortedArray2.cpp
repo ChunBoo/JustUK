89c890c77694d5021e19ab2d5dafa2c67534b50d
Chunbo Liu <chunboo.liu@gmail.com>
2024-01-31 22:43:48 +0800

    0131-added the example of binary search of 81

---
+++ binarysearch/3-81-searchInRotatedSortedArray2.cpp
@@ -0,0 +1,66 @@
+/**
+ * There is an integer array nums sorted in non-decreasing order (not necessarily with distinct values).
+
+Before being passed to your function, nums is rotated at an unknown pivot index k (0 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,4,4,5,6,6,7] might be rotated at pivot index 5 and become [4,5,6,6,7,0,1,2,4,4].
+
+Given the array nums after the rotation and an integer target, return true if target is in nums, or false if it is not in nums.
+
+You must decrease the overall operation steps as much as possible.
+
+ 
+
+Example 1:
+
+Input: nums = [2,5,6,0,0,1,2], target = 0
+Output: true
+Example 2:
+
+Input: nums = [2,5,6,0,0,1,2], target = 3
+Output: false
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+nums is guaranteed to be rotated at some pivot.
+-104 <= target <= 104
+*/
+
+class Solution {
+public:
+    bool search(vector<int>& nums, int target) {
+        int n=nums.size();
+        if(n==1)
+            return nums[0]==target;
+        int l=0,r=n-1;
+
+        while(l<=r)
+        {
+            int m=l+(r-l)/2;
+            if(nums[m]==target)
+                return true;
+            if(nums[l]==nums[m]&&nums[m]==nums[r])
+            {
+                ++l;
+                --r;
+            }
+            else if(nums[m]<nums[l])
+            {
+                if(target>nums[m]&&target<=nums[n-1])
+                    l=m+1;
+                else
+                    r=m-1;
+            }
+            else //nums[m]>nums[l]
+            {
+                if(target>=nums[l]&&target<nums[m])
+                    r=m-1;
+                else
+                    l=m+1;
+            }
+
+        }
+        return false;
+    }
+};
