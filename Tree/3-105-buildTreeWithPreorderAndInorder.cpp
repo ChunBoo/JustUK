0ed7c48959c5c31c32db53bd64d2d5d207f4edd3
chunboo <chunboo.liu@gmail.com>
2024-02-20 23:19:54 +0800

    0220 added the binary example for the 105

---
+++ Tree/3-105-buildTreeWithPreorderAndInorder.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        const int n=preorder.size();
+        if(n==1)
+            return new TreeNode(preorder[0]);
+        
+        TreeNode* root=new TreeNode(preorder[0]);
+        stack<TreeNode*> st;
+
+        st.push(root);
+        int inorderIndex=0;
+        for(int i=1;i<n;++i){
+            int preorderVal=preorder[i];
+            TreeNode* node=st.top();
+            if(node->val!=inorder[inorderIndex]){
+                node->left=new TreeNode(preorderVal);
+                st.push(node->left);
+            }
+            else{
+                while(!st.empty()&&st.top()->val==inorder[inorderIndex]){
+                    node=st.top();
+                    st.pop();
+                    ++inorderIndex;
+                }
+                node->right=new TreeNode(preorderVal);
+                st.push(node->right);
+            }
+        }
+        return root;
+    }
+};
