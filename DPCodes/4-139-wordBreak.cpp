b75901589e8a3a429ef2dbdf1806ea76d5d16a42
chunboo <chunboo.liu@gmail.com>
2024-02-24 22:34:20 +0800

    0224-added the memorization example of 139

---
+++ DPCodes/4-139-wordBreak.cpp
@@ -0,0 +1,64 @@
+
+/**
+ * @brief Given a string s and a dictionary of strings wordDict, return true if s can be segmented into a space-separated sequence of one or more dictionary words.
+
+Note that the same word in the dictionary may be reused multiple times in the segmentation.
+
+ 
+
+Example 1:
+
+Input: s = "leetcode", wordDict = ["leet","code"]
+Output: true
+Explanation: Return true because "leetcode" can be segmented as "leet code".
+Example 2:
+
+Input: s = "applepenapple", wordDict = ["apple","pen"]
+Output: true
+Explanation: Return true because "applepenapple" can be segmented as "apple pen apple".
+Note that you are allowed to reuse a dictionary word.
+Example 3:
+
+Input: s = "catsandog", wordDict = ["cats","dog","sand","and","cat"]
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 300
+1 <= wordDict.length <= 1000
+1 <= wordDict[i].length <= 20
+s and wordDict[i] consist of only lowercase English letters.
+All the strings of wordDict are unique.
+ * 
+ */
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        words_=set<string>{wordDict.begin(),wordDict.end()};
+        // for(const string& w:wordDict)
+        //     cached_.emplace(w,true);
+        return dp(s);
+    }
+private:
+    set<string> words_;
+    map<string,bool> cached_;
+    bool dp(const string& s){
+        if(cached_.count(s))
+            return cached_[s];
+        if(words_.count(s))
+            return cached_[s]=true;
+        //int minLen=min(20,int(s.size()));
+        for(int i=1;i<s.length();++i)
+        {
+            string preStr=s.substr(0,i);
+            string postStr=s.substr(i);
+            if(words_.count(preStr) && dp(postStr))
+            {
+                cached_[s]=true;
+                return true;
+            }
+        }
+        return cached_[s]=false;
+    }
+};
