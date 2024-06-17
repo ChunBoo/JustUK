4bd657fb7aef1a009e46cc0d382e684af73e888b
chunboo <chunboo.liu@gmail.com>
2024-06-17 22:06:14 +0800

    0617-added the two pointers example of 522 and check the subsequence example of 105

--- 105-twoPointersForCheckingSubString.cpp
+++ 105-twoPointersForCheckingSubString.cpp
@@ -3,6 +3,8 @@
 #include<string>
 #include<iostream>
 
+using namespace std;
+
 bool isSubString(const std::string& s, const std::string& t)
 {
     if(s.empty())
@@ -22,7 +24,22 @@ bool isSubString(const std::string& s, const std::string& t)
     return si==ls;
 }
 
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(t.size()==0&&s.size()>0)  //empty string is one substring 
+            return false;
 
+        int _s=0,_t=0;
+        while(_s<s.size()&&_t<t.size()){
+            if(s[_s]==t[_t]){
+                ++_s;
+            }
+            ++_t;
+        }
+        return _s==s.size();
+    }
+};
 int main()
 {
     std::string s{""};
