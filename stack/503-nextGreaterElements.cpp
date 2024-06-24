c6c87b560bf694299fcd8a553e7dd9d43a541cf7
chunboo <chunboo.liu@gmail.com>
2024-06-24 22:49:29 +0800

    0624-added new method of stack issue of 503

--- stack/503-nextGreaterElements.cpp
+++ stack/503-nextGreaterElements.cpp
@@ -47,3 +47,23 @@ public:
         return nums;
     }
 };
+
+class Solution {
+public:
+    vector<int> nextGreaterElements(vector<int>& nums) {
+        const int n=nums.size();
+        vector<int> ans(n,-1);
+        stack<int> st;
+        for(int i=2*n-1;i>=0;i--){
+            int x=nums[i%n];
+            while(!st.empty()&&x>=st.top()){
+                st.pop();
+            }
+            if(i<n&&!st.empty()){
+                ans[i]=st.top();
+            }
+            st.push(x);
+        }
+        return ans;
+    }
+};
