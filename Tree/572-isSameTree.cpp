89ce1d3535c8aa2e7cd360334a89db1adff581d9
chunboo <chunboo.liu@gmail.com>
2024-08-04 18:36:11 +0800

    0804 added the dfs example of binary tree of 572

--- Tree/572-isSameTree.cpp
+++ Tree/572-isSameTree.cpp
@@ -37,3 +37,39 @@ public:
         return isSameTree(root,subRoot)||isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
     }
 };
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        return dfs(root,subRoot);
+    }
+
+private:
+    bool check(TreeNode *o, TreeNode *t){
+        if(!o&&!t){
+            return true;
+        }
+        if((o&&!t||(!o&&t)||(o->val!=t->val))){
+            return false;
+        }
+        return check(o->left,t->left)&&check(o->right,t->right);;
+    }
+    bool dfs(TreeNode *o, TreeNode *t){
+        if(!o){
+            return false;
+        }
+        return check(o,t)||dfs(o->left,t)||dfs(o->right,t);
+    }
+};
